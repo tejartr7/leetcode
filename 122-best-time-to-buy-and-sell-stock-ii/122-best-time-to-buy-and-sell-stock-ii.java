2e9d5439b1c459bc293ea990dd7a4cf7acc4962d
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-06-12 13:16:44 +0530

    Time: 1 ms (94.93%), Space: 44.3 MB (44.76%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+       
+        int len = prices.length;
+        int profit = 0;        
+        for(int i = 1; i < len; i++) {
+            int todayPrice = prices[i];
+            int yesterdayPrice = prices[i - 1];
+            if(todayPrice > yesterdayPrice) profit += todayPrice - yesterdayPrice;            
+        }
+        return profit;
+    }
+}
