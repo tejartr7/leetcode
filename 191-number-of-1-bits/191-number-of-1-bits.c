1b094f45c2544a3643a6bc5a3b3bc48828dc20dd
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-06-18 23:52:23 +0530

    Time: 0 ms (100.00%), Space: 5.4 MB (92.13%) - LeetHub

---
+++ 191-number-of-1-bits/191-number-of-1-bits.c
@@ -0,0 +1,17 @@
+int hammingWeight(uint32_t n) {
+  int count=0;
+        int k=n;
+        int s;
+        while(n>0)
+        {
+            
+           
+            if(n%2==1)
+            {
+                count++;
+            }
+             n=n/2;
+            s=0;
+        }
+        return count;
+}
