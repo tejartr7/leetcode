e7825e290e615932e5116304ffa90fa9a846cc64
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-06-20 20:51:45 +0530

    Time: 1 ms (74.91%), Space: 43.4 MB (28.23%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -47,13 +47,13 @@ class Solution {
            }
            curr=curr.next;
        }
-        if(list1!=null)
+        while(list1!=null)
         {
             curr.next=list1;
             list1=list1.next;
             curr=curr.next;
         }
-        if(list2!=null)
+        while(list2!=null)
         {
             curr.next=list2;
            list2=list2.next;
