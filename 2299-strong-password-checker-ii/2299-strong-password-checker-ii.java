525fcc412912a3e89341a98dff2eb129589ad841
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-06-21 23:21:43 +0530

    Time: 2 ms (62.22%), Space: 42.4 MB (29.01%) - LeetHub

---
+++ 2299-strong-password-checker-ii/2299-strong-password-checker-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean strongPasswordCheckerII(String a) {
+      int i,j,l=a.length();
+        boolean num=true,spec=true,lower=true,upper=true;
+        boolean adj=false;
+       if(l<8)
+           return false;
+      for(i=0;i<l;i++)
+      {
+          char c=a.charAt(i);
+          String s=Character.toString(c);
+          if(i+1<l && a.charAt(i)==a.charAt(i+1))
+              return false;
+          if(c>='0' && c<='9')
+              num=false;
+          if(c>='a' && c<='z')
+              lower=false;
+           if(c>='A' && c<='Z')
+              upper=false;
+           if("!@#$%^&*()-+".contains(s))
+              spec=false;
+          
+      }
+        if(!num && !spec && !lower &&!upper)
+            return true;
+        return false;
+    }
+}
