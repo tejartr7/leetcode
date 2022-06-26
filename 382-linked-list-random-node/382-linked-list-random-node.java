473bfe0b9d199463742ecb4927fc65e00e2a9658
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-06-26 20:53:10 +0530

    Time: 31 ms (5.92%), Space: 49.8 MB (72.55%) - LeetHub

--- 382-linked-list-random-node/382-linked-list-random-node.java
+++ 382-linked-list-random-node/382-linked-list-random-node.java
@@ -1,19 +1,25 @@
 class Solution {
-    private ArrayList<Integer> range = new ArrayList<>();
+    private ListNode head;
 
     /** @param head The linked list's head.
         Note that the head is guaranteed to be not null, so it contains at least one node. */
     public Solution(ListNode head) {
-        while (head != null) {
-            this.range.add(head.val);
-            head = head.next;
-        }
+        this.head = head;
     }
 
     /** Returns a random node's value. */
     public int getRandom() {
-        int pick = (int)(Math.random() * this.range.size());
-        return this.range.get(pick);
+        int scope = 1, chosenValue = 0;
+        ListNode curr = this.head;
+        while (curr != null) {
+            // decide whether to include the element in reservoir
+            if (Math.random() < 1.0 / scope)
+                chosenValue = curr.val;
+            // move on to the next node
+            scope += 1;
+            curr = curr.next;
+        }
+        return chosenValue;
     }
 }
 /**
