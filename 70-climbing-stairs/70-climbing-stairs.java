2960adfb75a1fa47d43d7d719deede2e9ac2a771
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-06-27 08:09:56 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (56.71%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+ 
+    
+    public int climbStairs(int n) {
+   
+        
+        if(n==1)
+            return 1;
+        int a=1;
+        int b=1;
+        for(int i=2;i<=n;i++){
+            b=a+b;
+            a=b-a;
+            
+        }
+        return b;
+    }
+}
