ec796b777baa8e8594a17fcbcecee6297877c631
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-06-29 15:36:41 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (96.12%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.java
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -3,10 +3,7 @@ class Solution {
       int i;
       char c[]=s.toCharArray();
       int l=c.length;
-        if(l==1 && c[0]=='1')
-            return true;
-       if(l==2 && c[0]=='1')
-           return true;
+      
        for(i=1;i<l;i++)
        {
            if(c[i]=='1' && c[i-1]=='0')
