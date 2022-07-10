60b95c61f3d82e1b77689df6da4560c3965e794f
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-07-10 11:34:59 +0530

    Time: 2 ms (32.47%), Space: 44 MB (24.69%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int minCostClimbingStairs(int[] cost) {
-      if (cost.length == 2) // base
-            return Math.min(cost[0], cost[1]);
+    public int minCostClimbingStairs(int[] c) {
+     int l=c.length;
+     int i;
+     int d[]=new int[l+1];
+     if(l==2)
+         return Math.min(c[0],c[1]);
+     for(i=2;i<l+1;i++)
+     {
+         d[i]=Math.min(d[i-1]+c[i-1],d[i-2]+c[i-2]);
+     }
         
-        int[] dp = new int[cost.length + 1]; // the one for the top
-        dp[0] = 0;
-        dp[1] = 0; // we can reach index 0 or 1 freely
+        return d[l];
         
-        for (int i = 2; i < dp.length; i++) {
-			// To reach index i, the prev index could only be i - 2 or i - 1. Note: you should add the cost of climb
-            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]); 
-        }
         
-        return dp[dp.length - 1];  
     }
 }
