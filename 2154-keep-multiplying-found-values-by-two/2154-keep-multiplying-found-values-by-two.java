8ebbc23fddcae19316259f4542c6e11f0e53bdcc
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-07-16 16:50:23 +0530

    Time: 12 ms (5.19%), Space: 45.9 MB (15.16%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.java
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.java
@@ -1,10 +1,13 @@
 class Solution {
     public int findFinalValue(int[] nums, int original) {
+        Arrays.sort(nums);
         Set<Integer> a=new HashSet<>();
         int i;
         int l=nums.length;
         for(i=0;i<l;i++)
+        {   if(!a.contains(nums[i]))
             a.add(nums[i]);
+        }
         while(a.contains(original))
         {
             original*=2;
