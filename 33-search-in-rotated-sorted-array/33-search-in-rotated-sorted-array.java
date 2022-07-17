d46c6ac018fdde402bec20565b7db02db35a369c
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-07-17 19:47:23 +0530

    Time: 1 ms (74.47%), Space: 42.2 MB (73.47%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,15 +1,14 @@
 class Solution {
     public int search(int[] nums, int target) {
-       List<Integer> a=new ArrayList<Integer>();
-        int i;
-        int l=nums.length;
-        for(i=0;i<l;i++)
-        {
-            a.add(nums[i]);
-            
-        }
-        
-        return a.indexOf(target);
+      //HashMap<Integer,Integer> a=new HashMap<>();
+      int i;
+      int l=nums.length;
+      for(i=0;i<l;i++)
+      {
+          if(nums[i]==target)
+              return i;
+      }
+        return -1;
        
     }
 }
