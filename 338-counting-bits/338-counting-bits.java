bce8df9dc09181992bac4d521a325cc67d011b9b
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-07-17 21:39:41 +0530

    Time: 15 ms (13.89%), Space: 58.5 MB (5.02%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,27 +1,24 @@
 class Solution {
     public int[] countBits(int n) {
-        int i=0;
-      
-        int []a=new int[n+1];
-        while(i<=n)
-        {
-           String s=Integer.toBinaryString(i);
-             int count=0;
-              int ii;
-            int l=s.length();
-            for(ii=0;ii<l;ii++)
-            {
-                char aa=s.charAt(ii);
-                if(aa=='1')
-                    count++;
-                
-            }
-            a[i]=count;
-            count=0;
-            i++;
-            
-        
-        }
+          int i=0;
+          int a[]=new int[n+1];
+          while(i<=n)
+          {
+              int count=0;
+              int j;
+              String s=Integer.toBinaryString(i);
+              int l=s.length();
+              for(j=0;j<l;j++)
+              {
+                  char c=s.charAt(j);
+                  if(c=='1')
+                      count++;
+                  
+              }
+              a[i]=count;
+              count=0;
+              i++;
+          }
         return a;
     }
 }
