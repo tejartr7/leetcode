b7b1cb0fe3f75d66651c8a4f3b147e255696509e
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-07-21 16:09:56 +0530

    Time: 2 ms (46.26%), Space: 53.8 MB (73.54%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -0,0 +1,45 @@
+class Solution {
+    private void make(int aa[][],int x,int y)
+    {
+        int l=aa.length; 
+        int b=aa[0].length;
+        int i;
+        for(i=0;i<l;i++)
+        {
+            aa[i][y]=0;
+        }
+        for(i=0;i<b;i++)
+        {
+            aa[x][i]=0;
+        }
+        
+    }
+  public void setZeroes(int[][] matrix) {
+        int l=matrix.length; 
+        int b=matrix[0].length;
+        int i,j;
+        int a[][]=new int[l][b];
+        for(i=0;i<l;i++)
+        {
+            for(j=0;j<b;j++)
+            {
+                a[i][j]=matrix[i][j];
+            }
+        }
+          for(i=0;i<l;i++)
+        {
+            for(j=0;j<b;j++)
+            {
+                if(a[i][j]==0)
+                {
+                    make(matrix,i,j);
+            
+                }
+            }
+              
+              
+        }
+      
+        
+  }
+}
