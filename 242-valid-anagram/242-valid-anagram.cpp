7aae9e2cf6b9ca03567fe3b2609c632ca7880874
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-07-28 15:17:12 +0530

    Time: 7 ms (87.80%), Space: 7.3 MB (77.97%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,17 +1,26 @@
-class Solution {
+class Solution
+{
 public:
-   bool isAnagram(string &s, string &t) {                
-        vector<char>arr(26);
-        for(char c:s) // increment each character frequency
-            ++arr[c-97];
-        for(char c:t) // decrease each character frequency
-            --arr[c-97];
-	    // if frequency of all 26 characters is 0, then both string have equal number of each character
-		//  and hence, it is an anagram, else return false
-        for(char ch:arr) 
-            if(ch)
+    bool isAnagram(string s, string t)
+    {
+        if (s.length() != t.length())
+        {
+            return false;
+        }
+        vector<int> freq(26, 0);
+        int n = s.length();
+        for (int i = 0; i < n; i++)
+        {
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
+        }
+        for (int i = 0; i < 26; i++)
+        {
+            if (freq[i] != 0)
+            {
                 return false;
-				
+            }
+        }
         return true;
-}
+    }
 };
