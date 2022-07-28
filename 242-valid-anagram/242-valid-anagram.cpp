fc6c796bb9986e453975954df10a64574ec39d0d
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-07-28 15:13:26 +0530

    Time: 25 ms (16.79%), Space: 7.4 MB (45.59%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,21 +1,12 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        char a[]=s.toCharArray();
-         char b[]=t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        int l=s.length();
-        int count=0;
-        int l1=t.length();
-        if(l!=l1)
+public:
+    bool isAnagram(string s, string t) 
+    {
+		// If size is not equal we can declare straight away, that its not an anagram.
+        if (s.size() != t.size())
             return false;
-        for(int i=0;i<l;i++)
-        {
-            if(a[i]==b[i])
-                count++;
-            else
-                return false;
-        }
-        return true;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return (s == t);
     }
-}
+};
