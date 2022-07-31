648badaf3d7e91bfa5b0adf511e75b3d83da88e1
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-07-31 12:03:51 +0530

    Time: 126 ms (72.17%), Space: 130.7 MB (41.40%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.java
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.java
@@ -4,31 +4,22 @@ class NumArray {
     public NumArray(int[] nums) {
         int i,l=nums.length;
         a=new int[l];
-        
         for(i=0;i<l;i++)
         {
             a[i]=nums[i];
             total+=a[i];
         }
-        
-        
     }
-    
     public void update(int index, int val) {
-       
-        total=total+val-a[index];
+       total=total+val-a[index];
              a[index]=val;
     }
     
     public int sumRange(int left, int right) {
         int sum=total;
         int i;
-        int start=left;
-        int end=right;
         for(i=0;i<left;i++)
-        {
-            sum-=a[i];
-        }
+        sum-=a[i];
         for(i=right+1;i<a.length;i++)
             sum-=a[i];
         return sum;
