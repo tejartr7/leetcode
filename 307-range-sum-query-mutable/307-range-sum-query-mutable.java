f33e859dcfef016188ec207bb96f9cb5c9697d84
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-07-31 15:32:08 +0530

    Time: 148 ms (52.19%), Space: 128.7 MB (53.83%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.java
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.java
@@ -18,11 +18,21 @@ class NumArray {
     }
     public int sumRange(int left, int right) {
         int sum=total;
-     
+      int m=right-left+1;
+    int n=l+left-right-1;
+        if(m>n){
         for(i=0;i<left;i++)
         sum-=a[i];
         for(i=right+1;i<a.length;i++)
             sum-=a[i];
+        }
+        else
+        {
+            sum=0;
+            for(i=left;i<=right;i++)
+                sum+=a[i];
+        }    
+        
         return sum;
     }
 }
