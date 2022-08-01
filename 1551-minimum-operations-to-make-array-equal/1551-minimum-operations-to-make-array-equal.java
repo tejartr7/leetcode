2199abe087c31b67408cdba4873b0ff50506191c
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-08-01 21:24:52 +0530

    Time: 2 ms (45.58%), Space: 41.2 MB (45.43%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.java
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.java
@@ -1,15 +1,12 @@
 class Solution {
     public int minOperations(int n) {
-        int a[]=new int[n];
         int i;
-        for(i=0;i<n;i++)
-            a[i]=2*i+1;
         int sum=0;
         int start=0;
         int end=n-1;
         while(start<end)
         {
-            sum+=(a[end]-a[start])/2;
+            sum+=end-start;
             end--;
             start++;
         }
