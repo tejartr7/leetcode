0f371de305a3ec4800411f7da29a7e931a991cdf
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-08-04 16:12:12 +0530

    Time: 1 ms (42.82%), Space: 42.9 MB (20.90%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,17 +1,41 @@
 class Solution {
-    public void sortColors(int[] nums) {
-        int i,j;
-        int temp;
-        for(i=0;i<nums.length;i++)
+   public static void merge(int arr[], int si, int mid, int ei) {
+int merged [] = new int [ei-si+1] ;
+int idx1 = si;
+int idx2 = mid+1;
+int x = 0;
+while (idx1 <= mid && idx2 <= ei) {
+if(arr[idx1] <= arr [idx2]) {
+merged [x++] = arr[idx1++] ;
+                   }
+else {
+merged [x++] = arr[idx2++] ;
+}
+                   }
+while (idx1<= mid)
+{merged [x++] = arr[idx1++] ;
+}
+while ( idx2 <= ei) {
+merged [x++] = arr[idx2++];
+}
+for(int i=0, j=si; i<merged. length; i++, j++) {
+arr[j] = merged [i];
+}
+}
+    private void mergesort(int arr[],int a,int b)
+    {
+        if(a<b)
         {
-            for(j=i+1;j<nums.length;j++)
-            {if(nums[i]>nums[j]){
-                temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
-            }
-            }
+            int mid=a+(b-a)/2;
+            mergesort(arr,a,mid);
+            mergesort(arr,mid+1,b);
+            merge(arr,a,mid,b);
         }
+    }
+    public void sortColors(int[] nums) {
+       int i;
+       int start=0,end=nums.length-1;
+        mergesort(nums,start,end); 
       
         
         
