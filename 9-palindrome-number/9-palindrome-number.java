e41cd1c36d424f2025352e1830e3d930446732c3
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-08-04 11:07:54 +0530

    Time: 14 ms (63.12%), Space: 44.7 MB (53.97%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -3,6 +3,8 @@ class Solution {
         int k=0;
         int l;
         int o=x;
+        if(x<0)
+            return false;
         while(x>0)
         {
             l=x%10;
@@ -11,10 +13,9 @@ class Solution {
         }
         if(o==0)
         return true;
-        if(o==k && o>0)
+        if(o==k)
             return true;
-        
-        else
+           
             return false;
         
     }
