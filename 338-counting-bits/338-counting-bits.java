96496e4f8042a44160da211c90e4d03dbd99771e
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-08-11 17:58:30 +0530

    Time: 1 ms (99.97%), Space: 46.5 MB (92.82%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -4,10 +4,7 @@ class Solution {
           int a[]=new int[n+1];
           for(i=0;i<=n;i++)
           {
-              if(i%2==0)
-                  a[i]=a[i/2];
-              else
-                  a[i]=a[i/2]+1;
+              a[i]=Integer.bitCount(i);
           }
         return a;
     }
