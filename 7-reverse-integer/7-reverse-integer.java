f610b63335cff2e93a30994eb6e6dfa6b5c32c25
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-08-11 14:30:25 +0530

    Time: 1 ms (100.00%), Space: 41.4 MB (56.97%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.java
+++ 7-reverse-integer/7-reverse-integer.java
@@ -1,24 +1,16 @@
 class Solution {
     public int reverse(int x) {
         long r=0;
-        
-      int l;
-    
+        int l;
         while(x!=0)
-        {
-            l=x%10;
-            r=r*10+l;
-        
-            x=x/10;
-           
-            
+        {l=x%10;
+         r=r*10+l;
+         x=x/10;
         }
-       
-       
-         if(r>Integer.MAX_VALUE|| r<Integer.MIN_VALUE){
+        if(r>Integer.MAX_VALUE|| r<Integer.MIN_VALUE){
             return 0;
         }
-            return (int)r;
+        return (int)r;
     
         
     }
