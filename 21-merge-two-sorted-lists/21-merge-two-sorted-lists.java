1d0000429c8f2b1bb48183400ade2c1ec41d4bc1
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-08-12 13:50:36 +0530

    Time: 1 ms (80.19%), Space: 42.9 MB (60.23%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -6,47 +6,27 @@
  *     ListNode() {}
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }  ListNode dummyHead = new ListNode(-1);
-        ListNode node = dummyHead;
-        
-        while(list1!=null && list2!=null) {
-            
-            if(list1.val < list2.val) {
-                node.next = list1;
-                list1 = list1.next;
-            } else {
-                node.next = list2;
-                list2 = list2.next;
-            }
-            node = node.next;
-        }
-        
-        if(list1!=null) {
-            node.next = list1;
-        } else if(list2!=null) {
-            node.next = list2;
-        }
-        
-        return dummyHead.next;
+ * }
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-       ListNode a=new ListNode(-1);
-       ListNode curr=a;
-       while(list1!=null && list2!=null)
-       {
-           if(list1.val<list2.val)
-           {
-               curr.next=list1;
-               list1=list1.next;
-           }
-           else
-           {
-               curr.next=list2;
-               list2=list2.next;
-           }
-           curr=curr.next;
-       }
+        ListNode a=new ListNode(-1);
+        ListNode curr=a;
+        while(list1!=null && list2!=null)
+        {
+            if(list1.val<list2.val)
+            {
+                curr.next=list1;
+                list1=list1.next;
+                
+            }
+            else
+            {
+                curr.next=list2;
+                list2=list2.next;
+            }
+            curr=curr.next;
+        }
         while(list1!=null)
         {
             curr.next=list1;
@@ -56,7 +36,7 @@ class Solution {
         while(list2!=null)
         {
             curr.next=list2;
-           list2=list2.next;
+            list2=list2.next;
             curr=curr.next;
         }
         return a.next;
