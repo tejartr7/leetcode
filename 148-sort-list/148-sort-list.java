7b5b833ee3f09a2c53a186c041be5abf74c51039
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-08-14 18:45:35 +0530

    Time: 18 ms (57.98%), Space: 75.6 MB (63.83%) - LeetHub

---
+++ 148-sort-list/148-sort-list.java
@@ -0,0 +1,42 @@
+
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode mid = getMid(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        return merge(left, right);
+    }
+
+    ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dummyHead = new ListNode();
+        ListNode tail = dummyHead;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+                tail = tail.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+                tail = tail.next;
+            }
+        }
+        tail.next = (list1 != null) ? list1 : list2;
+        return dummyHead.next;
+    }
+
+    ListNode getMid(ListNode head) {
+         ListNode a=head.next.next;
+        ListNode b=head;
+        while( a!=null && a.next!=null)
+        {
+            a=a.next.next;
+            b=b.next;
+        }
+        ListNode k=b.next;
+        b.next=null;
+        return k;
+    }
+}
