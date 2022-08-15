cea35553f1b04100908d50b12a69b2cf420c4a9b
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-08-15 17:15:34 +0530

    Time: 29 ms (6.12%), Space: 57.7 MB (61.92%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,52 +1,37 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int []k=new int[nums.length];
-        int i,j;
+       Map<Integer,Integer> a=new HashMap<>();
+        int i;
+        int l=nums.length;
         int pro=1;
-        if(nums.length==2 && nums[0]==1 && nums[1]==0)
+        a.put(0,0);
+        for(i=0;i<l;i++)
         {
-            k[0]=0;
-            k[1]=1;
-            return k;
+            a.put(nums[i],a.getOrDefault(nums[i],0)+1);
+            if(nums[i]!=0)
+                pro*=nums[i];
+            
         }
-        if(nums.length==3 && nums[0]==9 && nums[1]==0 && nums[2]==-2)
+        if(a.get(0)>1)
         {
-            k[0]=0;
-            k[1]=-18;
-            k[2]=0;
-            return k;
+            Arrays.fill(nums,0);
+            return nums;
         }
-          if(nums.length==4 && nums[0]==2 && nums[1]==3 && nums[2]==5 && nums[3]==0)
+        for(i=0;i<l;i++)
         {
-            k[0]=0;
-            k[1]=0;
-            k[2]=0;
-              k[3]=30;
-            return k;
-        }
-        if(nums.length==5 && nums[0]==-1 && nums[1]==1 && nums[2]==0 && nums[3]==-3 && nums[4]==3 )
-        {
-           
-             k[0]=0;
-             k[1]=0;
-             k[2]=9;
-             k[3]=0;
-             k[4]=0;
-            return k;
-        }
-       
-        else{for(i=0;i<nums.length;i++)
-        {
-            pro=pro*nums[i];
-        }
-        for(i=0;i<nums.length;i++)
-        { if(nums[i]!=0){
-            k[i]=pro/nums[i];
-        }
-         else
-             k[i]=pro;
-        }
-        return k;
+            if(nums[i]!=0 && a.get(0)>0)
+            {
+                nums[i]=0;
             }
+            else if(nums[i]==0 && a.get(0)==1)
+            {
+                nums[i]=pro;
+            }
+            else if(nums[i]!=0 && a.get(0)==0)
+            {
+              nums[i]=pro/nums[i];
+            }
+        }
+        return nums;
     }
 }
