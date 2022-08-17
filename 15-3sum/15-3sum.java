450f7f6961c2f49028133a79a3d09e74264367e0
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-08-17 20:01:29 +0530

    Time: 38 ms (46.97%), Space: 59.5 MB (40.18%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -24,8 +24,7 @@ class Solution {
                     k--;
                     while(j<k && nums[j]==nums[j-1])
                     j++;
-                    while(j<k && nums[k]==nums[k+1])
-                    k--;    
+                     
                     
                     
                     
