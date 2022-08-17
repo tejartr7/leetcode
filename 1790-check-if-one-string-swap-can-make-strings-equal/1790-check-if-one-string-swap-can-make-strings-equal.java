5169849a5edf23f9b253d5183ae07837f4893a31
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-08-17 23:00:41 +0530

    Time: 1 ms (90.16%), Space: 42.3 MB (43.78%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
@@ -1,28 +1,29 @@
 class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
        
-        char a[]=s1.toCharArray();
-        char b[]=s2.toCharArray();
+        List<Integer> index=new ArrayList<>();
         int i;
-        int l=a.length;
-        int x=0;
-        for(i=0;i<l;i++)
+        int m=s1.length();
+        int n=s2.length();
+        if(m!=n)
+            return false;
+        if(s1.equals(s2))
+            return true;
+        for(i=0;i<m;i++)
         {
-            if(a[i]!=b[i])
-                x++;
+            if(s1.charAt(i)!=s2.charAt(i))
+            {
+                index.add(i);
+            }
         }
-        Arrays.sort(a);
-        Arrays.sort(b);
-        String k=new String(a);
-        String kk=new String(b);
-        if(!k.equals(kk))
-            return false;
-         if(s1.equals(s2))
-            return true;
-        else if(s1.length()!=s2.length())
-            return false;
-        else if(x==2)
-            return true;
+        if(index.size()==2)
+        {
+            int a=index.get(0);
+            int b=index.get(1);
+            if(s1.charAt(a)==s2.charAt(b) && s1.charAt(b)==s2.charAt(a))
+                return true;
+        }
+        
         return false;
         
     }
