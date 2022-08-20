da9701ce57f21e3fe056d1558cc51bc3cdcce691
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-08-20 18:01:01 +0530

    Time: 24 ms (5.21%), Space: 71.5 MB (5.05%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int i;
+        int l;
+        int n=nums.length;
+        if(k>n)
+            k%=n;
+        Map<Integer,Integer> map=new HashMap();
+        for(i=0;i<n;i++)
+            map.put(i,nums[i]);
+        for(i=0;i<n;i++)
+            nums[i]=map.remove((n-k+i)%n);
+    }
+}
