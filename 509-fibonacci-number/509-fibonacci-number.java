0136461ee7c77d970737bea215b4dcb5e7b462a1
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-08-23 15:51:23 +0530

    Time: 0 ms (100.00%), Space: 41 MB (32.82%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,10 +1,18 @@
 class Solution {
     public int fib(int n) {
-        if(n==0)
-            return 0;
+        int dp[]=new int[n+1];
         if(n==1)
             return 1;
-        else
-            return fib(n-1)+fib(n-2);
+        if(n==0)
+            return 0;
+            
+        dp[0]=0;
+        dp[1]=1;
+        int i;
+        for(i=2;i<n+1;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
