29e7186cbbdc30ccf1cfe9b3de97a83d314514b5
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-08-23 18:15:27 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (99.55%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,6 +1,6 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int n=matrix.length;
+         int n=matrix.length;
         int i;
         int j;
         for(i=0;i<(n+1)/2;i++)
@@ -9,13 +9,12 @@ class Solution {
             {
                 int temp=matrix[n-1-j][i];
                 matrix[n-1-j][i]=matrix[n-1-i][n-1-j];
-matrix[n-1-i][n-1-j]=matrix[j][n-1-i];
+                matrix[n-1-i][n-1-j]=matrix[j][n-1-i];
                 matrix[j][n-1-i]=matrix[i][j];
                 matrix[i][j]=temp;
                 
                 
             }
         }
-      
     }
 }
