34ca5fd4263c307e5e0be27982afbf76f69dd9ec
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-08-25 19:30:05 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (63.13%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -5,10 +5,13 @@ class Solution {
      int k;
     for(k=m+n-1;k>=0;k--)
     {
-        if((i>=0 &&j<0)||(i>=0&& nums1[i]>nums2[j]))
+        if(i>=0 &&j<0)
            {
                nums1[k]=nums1[i--];
            }
+		   else if(i>=0  && nums1[i]>nums2[j])
+		   {nums1[k]=nums1[i--];
+		   }
           else if(j>=0)
            {
                nums1[k]=nums2[j--];
