5c1bf2ebdc31e858486e380e688a2676947ef3f3
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-08-27 15:56:00 +0530

    Time: 9 ms (11.39%), Space: 42.1 MB (99.62%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,10 +1,10 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-         List<List<Integer>> res=new ArrayList<>();
-        res.add(new ArrayList<>());
+        List<List<Integer>> res=new ArrayList<>();
         int i,j;
         int n=nums.length;
+        res.add(new ArrayList<>());
         for(i=0;i<n;i++)
         {
             int l=res.size();
@@ -12,7 +12,8 @@ class Solution {
             {
                 List<Integer> temp=new ArrayList(res.get(j));
                 temp.add(nums[i]);
-                if(!res.contains(temp))
+                if(res.contains(temp))
+                    continue;
                 res.add(temp);
             }
         }
