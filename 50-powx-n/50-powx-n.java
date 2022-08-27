55278305793a1765f7b123015a46e27a8302cf07
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-08-27 13:58:00 +0530

    Time: 1 ms (89.85%), Space: 41.7 MB (90.15%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,5 +1,14 @@
 class Solution {
     public double myPow(double x, int n) {
-        return (double)Math.pow(x,n);
+      if(x==1.0 || n==0)
+          return 1;
+       
+        if(n%2==0)
+            return myPow(x*x,n/2);
+        else if(n%2==1)
+            return myPow(x,n-1)*x;
+	  
+         
+            return 1/myPow(x,-n);
     }
 }
