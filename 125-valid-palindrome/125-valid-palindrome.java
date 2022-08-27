81b1c9a62f0360f7d529f1e9d8040f69d28c7650
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-08-27 14:46:08 +0530

    Time: 3 ms (98.90%), Space: 42.2 MB (96.18%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,7 +1,26 @@
-class Solution {
+public class Solution {
     public boolean isPalindrome(String s) {
-       String ans=s.replaceAll("[^A-Za-z0-9]","").toLowerCase();
-        String rev=new StringBuffer(ans).reverse().toString();
-        return ans.equals(rev);
+        if (s.isEmpty()) {
+        	return true;
+        }
+        int head = 0, tail = s.length() - 1;
+        char cHead, cTail;
+        while(head <= tail) {
+        	cHead = s.charAt(head);
+        	cTail = s.charAt(tail);
+        	if (!Character.isLetterOrDigit(cHead)) {
+        		head++;
+        	} else if(!Character.isLetterOrDigit(cTail)) {
+        		tail--;
+        	} else {
+        		if (Character.toLowerCase(cHead) != Character.toLowerCase(cTail)) {
+        			return false;
+        		}
+        		head++;
+        		tail--;
+        	}
+        }
+        
+        return true;
     }
 }
