290124264853837d81cfe97c73298c14a834c0a3
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-08-28 16:27:20 +0530

    Time: 29 ms (20.44%), Space: 68.4 MB (89.80%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -8,11 +8,11 @@ class Solution {
             a.add(nums[i]);
         }
         int j=1;
-        while(j<=nums.length)
+        while(j<=nums.length+1)
         { if(!a.contains (j))
             return j;
             j++;
         }
-        return nums.length+1;
+        return -1;
     }
 }
