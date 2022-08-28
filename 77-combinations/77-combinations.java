d6fa0628430ead57c1d4ea1a9605f0761ce220c3
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-08-28 14:11:16 +0530

    Time: 79 ms (11.60%), Space: 54.8 MB (54.56%) - LeetHub

--- 77-combinations/77-combinations.java
+++ 77-combinations/77-combinations.java
@@ -1,32 +1,29 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> res=new ArrayList<>();
-        List<List<Integer>> result=new ArrayList<>();
-     
-        res.add(new ArrayList<>());
-        int []a=new int[n];
-        int i;
-        for(i=0;i<n;i++)
-            a[i]=i+1;
-        
-        int j;
-        for(i=0;i<n;i++)
-        {
-            int l=res.size();
-            for(j=0;j<l;j++)
-            {
-                List temp=new ArrayList(res.get(j));
-                temp.add(a[i]);
-               res.add(temp);
-            }
-        }
-        for(i=0;i<res.size();i++)
-        {
-            List<Integer> temp=new ArrayList(res.get(i));
-            if(temp.size()==k)
-                result.add(temp);
-        }
-       return result;
-        
+        List<List<Integer>> result = new ArrayList<>();            //used to store the result (all the possible combonations)
+        List<Integer> combinations = new ArrayList<>();            //use to store the combinations or we can say that subset
+
+        solveitforMe(1,n,k,combinations,result);                   //Actual recursive logic
+
+        return result;                
     }
+
+    private void solveitforMe(int num, int n, int k, List<Integer> combinations, List<List<Integer>> result){
+
+        if(num>n+1)                                         //OUR BASE CONDITION...if num is greater than the given n+1 then return (No more recusrive calls)
+            return;
+
+        if(combinations.size() == k){                       //ANOTHER BASE CONDITION... if temp list size i=is equals to the given k, then it means we one of our combinations
+            // System.out.println(combinations);
+            result.add(new ArrayList(combinations));        //Add that combination into the result list and return
+            return;
+        }
+
+        combinations.add(num);                              //Add(TAKE) that number (we two choose TAKE & DON'T TAKE)
+        solveitforMe(num+1,n,k,combinations,result);        //Do the recursive call further
+    
+    combinations.remove(combinations.size()-1);         //DON'T TAKE that number
+    solveitforMe(num+1,n,k,combinations,result);        // & Do the recursive call further
+    }
+
 }
