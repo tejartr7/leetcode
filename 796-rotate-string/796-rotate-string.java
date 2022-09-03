a09729983e772b431e542fdc8c501403b365f3db
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-09-03 22:41:18 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (40.01%) - LeetHub

--- 796-rotate-string/796-rotate-string.java
+++ 796-rotate-string/796-rotate-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-       String ans=s+s;
+        String ans=s+s;
         if(s.length()!=goal.length())
             return false;
         else if(ans.contains(goal))
