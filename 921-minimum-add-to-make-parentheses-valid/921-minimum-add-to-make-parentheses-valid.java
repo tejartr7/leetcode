4fef999ec95a5176e1ccf8584043ada89c560f01
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-09-11 11:06:10 +0530

    Time: 2 ms (51.33%), Space: 42 MB (49.95%) - LeetHub

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.java
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.java
@@ -3,7 +3,7 @@ class Solution {
         int ans = 0, bal = 0;
         for (int i = 0; i < S.length(); ++i) {
             bal += S.charAt(i) == '(' ? 1 : -1;
-            // It is guaranteed bal >= -1
+         
             if (bal == -1) {
                 ans++;
                 bal++;
