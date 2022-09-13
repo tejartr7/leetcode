97306b47c9af5c27196a709e712968c71b86023a
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-09-13 22:44:35 +0530

    Time: 12 ms (53.22%), Space: 42.4 MB (90.12%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.java
+++ 2325-decode-the-message/2325-decode-the-message.java
@@ -1,18 +1,15 @@
 class Solution {
     public String decodeMessage(String key, String message) {
-        String k=key.replaceAll(" ","");
         Map<Character,Character> a=new HashMap<>();
-       
-        int i,n=k.length();
-           key = key.replaceAll(" ", "");
-     
+        key = key.replaceAll(" ", "");
         char original = 'a';
+        int i;
         for (i = 0; i < key.length() ; i++) {
             if (!a.containsKey(key.charAt(i))){
                 a.put(key.charAt(i),original++);
             }
         }
-       // System.out.println(a.get('t'));
+      
         int n2=message.length();
         String y="";
         for(i=0;i<n2;i++)
