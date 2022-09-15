7c175e0667339a6cba49a7fafa7aa4b65afdabed
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-09-15 23:09:49 +0530

    Time: 2 ms (23.34%), Space: 41.7 MB (76.95%) - LeetHub

--- 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.java
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.java
@@ -1,18 +1,15 @@
 class Solution {
     public int minimumMoves(String s) {
-        int i,n=s.length();
-        int count=0;
-        char c[]=s.toCharArray();
-        for(i=0;i<n-2;i++)
-        { if(c[i]=='O')
-                continue;
-            c[i]='O';
-            c[i+1]='O';
-            c[i+2]='O';
-            count++;}
-        if(c[n-1]!='O' || c[n-2]!='O'||c[n-3]!='O')
-            count++;
-        return count;
+         int ans=0,i=0;
+        while(i<s.length()){
+            if(s.charAt(i)=='X'){
+                ans++;
+                i+=3;
+            }else{
+                i++;
+            }
+        }
+        return ans;
         
     }
 }
