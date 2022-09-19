5fd7556e6970787538c1e3b163657afae901bce6
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-09-19 21:08:54 +0530

    Time: 11 ms (7.94%), Space: 43.7 MB (5.88%) - LeetHub

--- 394-decode-string/394-decode-string.java
+++ 394-decode-string/394-decode-string.java
@@ -1,44 +1,43 @@
 class Solution {
    public String decodeString(String s) {
-        Stack<Character> stack = new Stack<>();
-
+        Stack<Character> ans = new Stack<>();
+        int i;
         for(char c : s.toCharArray()){
-
-            // Step2:if char == ']',start to find the (1)subStr;(2)k due to the formation of k[subStr]
-            if(c == ']'){
-                StringBuilder subStr = new StringBuilder();
-                String temp = new String();
-                int k;
-                //Step2-1:find the (1)subStr and remove the '[' in the stack
-                while(!stack.isEmpty() && stack.peek() != '['){
-                    subStr.append(stack.pop());
-                }
-                stack.pop();
-
-                //Step2-2:find the (2)k and remove the k in the stack. k is in range [1,300]
-                while(!stack.isEmpty() && Character.isDigit(stack.peek())){
-                    temp = stack.pop() + temp;
-                }
-                k = Integer.parseInt(temp);
-
-                //Step2-3:push the subStr into the stack
-                while(k-- > 0){
-                    for(int i = subStr.length() - 1;i >= 0;i--){
-                        stack.push(subStr.charAt(i));
-                    }
-                }
-            }else{
-                // Step1:put the char into the stack, except ']'
-                stack.push(c);
-            }
+            char ch=c;
+         
+          if(ch==']')
+          {   StringBuilder a=new StringBuilder();
+              String temp=new String();
+              int k;
+              while(!ans.isEmpty())
+              {
+                  char chh=ans.pop();
+                  if(chh=='[')
+                      break;
+                  a.append(chh);
+              }
+              while(!ans.isEmpty() && Character.isDigit(ans.peek()))
+              {
+                 temp=ans.pop()+temp; 
+              }
+               k = Integer.parseInt(temp);
+              while(k>0)
+              {   for(i=a.length()-1;i>=0;i--)
+              {
+                  ans.push(a.charAt(i));
+              }
+                  k--;
+              }
+          }
+            else
+                ans.push(c);
+           
         }
-
-        //Step3:reverse the stack
-        char[] result = new char[stack.size()];
-        int i = stack.size() - 1;
-        while(!stack.isEmpty()){
-            result[i--] = stack.pop();
-        }
-        return new String(result);
+       String res="";
+       while(!ans.isEmpty())
+       {
+           res=String.valueOf(ans.pop())+res;
+       }
+       return res;
    }
 }
