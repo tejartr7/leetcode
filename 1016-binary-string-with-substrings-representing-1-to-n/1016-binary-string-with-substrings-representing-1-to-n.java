72ef7c3006a1fc0f5504b74e466c381b9d4703ef
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-09-21 22:16:05 +0530

    Time: 4 ms (27.25%), Space: 41.7 MB (76.35%) - LeetHub

--- 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.java
+++ 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.java
@@ -1,10 +1,18 @@
 class Solution {
     public boolean queryString(String s, int n) {
+        Queue<String> x=new LinkedList<>();
+        //add 1 intially to the queue
+        x.add("1");
         while(n>0)
-        {
-            String x=Integer.toBinaryString(n);
-            if(!s.contains(x))
+        {   //remove the first element from the queue
+            String temp=x.remove();
+            //if s doesnt have the value of the front element pop
+            if(!s.contains(temp))
                 return false;
+            //then add 0 to temp and then add temp to x 
+            // similary add 1 to tempa nd add temp to x
+            x.add(temp+"0");
+            x.add(temp+"1");
             n--;
         }
         return true;
