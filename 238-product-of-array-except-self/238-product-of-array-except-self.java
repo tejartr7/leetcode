0bb86b7be9850fbe67b215b199a1f8a7195d9267
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-09-23 18:34:22 +0530

    Time: 1 ms (100.00%), Space: 50.1 MB (97.99%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,16 +1,13 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        
-        int i;
         int n=nums.length;
-       
+        int i;
         int res[]=new int[n];
         res[0]=nums[0];
         for(i=1;i<n;i++)
         {
             res[i]=res[i-1]*nums[i];
-          //  System.out.print(res[i]);
-        }       
+        }
         int pro=nums[n-1];
         res[n-1]=res[n-2];
         for(i=n-2;i>0;i--)
@@ -20,5 +17,6 @@ class Solution {
         }
         res[0]=pro;
         return res;
+        
     }
 }
