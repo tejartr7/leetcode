8322e9d5d70fb23bdff5573302a82612971e3374
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-09-29 19:44:10 +0530

    Time: 12 ms (51.22%), Space: 61.9 MB (77.47%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,19 +1,21 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-            int start=0;
-            int end=arr.length-1;
-            List<Integer> ans=new ArrayList<>();
-            while(end-start>=k)
-            {
-                int diff1=Math.abs(arr[start]-x);
-                int diff2=Math.abs(arr[end]-x);
-                if(diff1<=diff2)
-                end--;
-                else
+        int i,n=arr.length;
+        List<Integer> result=new ArrayList<>();
+        int start=0,end=n-1;
+        while(end-start>=k)
+        {
+            int diff1=Math.abs(arr[start]-x);
+            int diff2=Math.abs(arr[end]-x);
+            if(diff1>diff2)
                 start++;
-            }
-            while(start<=end)
-            ans.add(arr[start++]);
-            return ans;
+            else
+                end--;
+        }
+        while(start<=end)
+        {    result.add(arr[start]);
+             start++;}
+        
+        return result;
     }
 }
