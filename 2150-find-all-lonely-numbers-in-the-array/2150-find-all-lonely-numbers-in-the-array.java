6249440434606788cc6860587e44656fe9420d34
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-10-05 16:38:12 +0530

    Time: 63 ms (84.20%), Space: 141.8 MB (76.84%) - LeetHub

--- 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.java
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.java
@@ -15,9 +15,7 @@ class Solution {
             lonelyList.add(nums[0]);
         for(i=1;i<n-1;i++)
         {
-            if(nums[i]==nums[i-1] || nums[i]==nums[i+1])
-                continue;
-            if(nums[i]==nums[i-1]+1 || nums[i]==nums[i+1]-1)
+            if(nums[i]==nums[i-1] || nums[i]==nums[i+1] ||nums[i]==nums[i-1]+1 || nums[i]==nums[i+1]-1)
                 continue;
             else
                 lonelyList.add(nums[i]);
