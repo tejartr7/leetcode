0b202ad59100dca4e7c39397178eefa9726536e2
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-10-07 16:25:37 +0530

    Time: 1 ms (79.49%), Space: 40.8 MB (64.78%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.java
+++ 650-2-keys-keyboard/650-2-keys-keyboard.java
@@ -13,44 +13,27 @@ class Solution {
      }
      public boolean isPrime(int n)
     {
- 
-        // Check if number is less than
-        // equal to 1
         if (n <= 1)
             return false;
- 
-        // Check if number is 2
         else if (n == 2)
             return true;
- 
-        // Check if n is a multiple of 2
         else if (n % 2 == 0)
             return false;
- 
-        // If not, then just check the odds
         for (int i = 3; i <= Math.sqrt(n); i += 2) {
             if (n % i == 0)
                 return false;
         }
         return true;
     }
-    public boolean isPerfectSquare(int n)
-    {
-        int x=(int)Math.sqrt(n);
-        if(n==x*x)
-            return true;
-        return false;
-    }
     public int minSteps(int n) {
         int min=0;
         if(n==1)
             return 0;
-       
-        if((n&1)==0)
+        else if((n&1)==0)
         {
             return min+2+minSteps(n/2);
         }
-        if(isPrime(n))
+        else if(isPrime(n))
             return n;
         else
         {
