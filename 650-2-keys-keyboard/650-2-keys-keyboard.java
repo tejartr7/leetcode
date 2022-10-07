b1a488489f79bbbfa613c8eba42ccfe9f9ebfa17
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-10-07 16:31:00 +0530

    Time: 1 ms (79.49%), Space: 41.5 MB (27.34%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.java
+++ 650-2-keys-keyboard/650-2-keys-keyboard.java
@@ -1,45 +1,13 @@
 class Solution {
-     public int gpf(int n)
-     {   int ans=1;
-         int i;
-         for(i=2;i<n;i++)
-         {
-             if(n%i==0)
-             {
-                     ans=i;
-             }
-         }
-      return ans;
-     }
-     public boolean isPrime(int n)
-    {
-        if (n <= 1)
-            return false;
-        else if (n == 2)
-            return true;
-        else if (n % 2 == 0)
-            return false;
-        for (int i = 3; i <= Math.sqrt(n); i += 2) {
-            if (n % i == 0)
-                return false;
-        }
-        return true;
-    }
     public int minSteps(int n) {
-        int min=0;
-        if(n==1)
-            return 0;
-        else if((n&1)==0)
-        {
-            return min+2+minSteps(n/2);
+        int ans = 0, d = 2;
+        while (n > 1) {
+            while (n % d == 0) {
+                ans += d;
+                n /= d;
+            }
+            d++;
         }
-        else if(isPrime(n))
-            return n;
-        else
-        {
-            int x=gpf(n);
-            return minSteps(x)+n/x;
-        }    
-         
+        return ans;
     }
 }
