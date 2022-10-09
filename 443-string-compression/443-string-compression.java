205e4ff00d32df29c4ec4a3fc649ab4bc8c130b0
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-10-09 20:43:37 +0530

    Time: 14 ms (9.10%), Space: 42.7 MB (84.69%) - LeetHub

--- 443-string-compression/443-string-compression.java
+++ 443-string-compression/443-string-compression.java
@@ -4,7 +4,6 @@ class Solution {
         if(n<=1)
             return n;
         String s="";
-        String temp="1234567890";
         int count=1;
         for(i=1;i<n;i++)
         { 
@@ -20,9 +19,7 @@ class Solution {
                 s+=String.valueOf(chars[i-1]);
                 count=1;
             }
-            //System.out.print(count+" "+s+" ");
         }
-        
         if(chars[n-1]!=chars[n-2])
         {
             s+=String.valueOf(chars[n-1]);
@@ -33,7 +30,6 @@ class Solution {
         for(i=0;i<s.length();i++)
         {
            chars[i]=s.charAt(i);
-            
         }
         return s.length();
     }
