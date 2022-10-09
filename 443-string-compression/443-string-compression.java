69049d68b9c6535693e7d03e4d9efd9d0b9532d1
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-10-09 20:43:12 +0530

    Time: 27 ms (5.49%), Space: 47.1 MB (6.44%) - LeetHub

---
+++ 443-string-compression/443-string-compression.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int compress(char[] chars) {
+        int i,n=chars.length;
+        if(n<=1)
+            return n;
+        String s="";
+        String temp="1234567890";
+        int count=1;
+        for(i=1;i<n;i++)
+        { 
+            if(chars[i-1]==chars[i])
+            {
+                count++;
+            
+            }
+            else
+            {   if(count!=1)
+                s+=String.valueOf(chars[i-1])+count;
+                if(count==1)
+                s+=String.valueOf(chars[i-1]);
+                count=1;
+            }
+            //System.out.print(count+" "+s+" ");
+        }
+        
+        if(chars[n-1]!=chars[n-2])
+        {
+            s+=String.valueOf(chars[n-1]);
+        }
+        else
+            s+=String.valueOf(chars[n-2])+count;
+        System.out.println(s);
+        for(i=0;i<s.length();i++)
+        {
+           chars[i]=s.charAt(i);
+            
+        }
+        return s.length();
+    }
+}
