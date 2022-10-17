4a01ca192cebd60eb6d2a9f72ceaf73aa224ff5d
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-10-17 18:51:38 +0530

    Time: 5 ms (37.22%), Space: 42.1 MB (54.96%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,9 +1,15 @@
 class Solution {
-    public boolean checkIfPangram(String sentence) {
-      if(sentence.length()<26) return false;
-
-        for (int i=0;i<26;i++){
-            if(!sentence.contains(Character.toString(i+97))) return false;
+    public boolean checkIfPangram(String s) {
+        boolean a[]=new boolean[26];
+        int i,n=s.length();
+        for(i=0;i<n;i++)
+        {
+            a[s.charAt(i)-'a']=true;
+        }
+        for(i=0;i<26;i++)
+        {
+            if(!a[i])
+                return false;
         }
         return true;
     }
