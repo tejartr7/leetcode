05291add4073dc4fe6b50a4a140069887e331f96
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-10-21 23:51:55 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (82.46%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,42 +1,45 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] a) {
         int amin=0,amax=a.length-1,bmin=0,bmax=a[0].length-1;
-        int r=a.length,l=a[0].length;
-        List<Integer> list=new ArrayList<>();    
+        List<Integer> list=new ArrayList<>();  
+        boolean visited[][]=new boolean[amax+1][bmax+1];
         while(amin<=amax && bmin<=bmax)
         {
             int i,j;
             i=amin;
             for(j=bmin;j<=bmax;j++)
-            {
+            {   if(!visited[i][j]){
                 list.add(a[i][j]);
+                visited[i][j]=true;
+            }
             }
             amin++;
             j=bmax;
-            if(list.size()==r*l)
-                return list;
             for(i=amin;i<=amax;i++)
             {
+               if(!visited[i][j]){
                 list.add(a[i][j]);
+                visited[i][j]=true;
             }
-            if(list.size()==r*l)
-                return list;
+            }
             bmax--;
             i=amax;
             for(j=bmax;j>=bmin;j--)
             {
+                if(!visited[i][j]){
                 list.add(a[i][j]);
+                visited[i][j]=true;
             }
-            if(list.size()==r*l)
-                return list;
+            }
             amax--;
             j=bmin;
             for(i=amax;i>=amin;i--)
             {
+                if(!visited[i][j]){
                 list.add(a[i][j]);
+                visited[i][j]=true;
             }
-            if(list.size()==r*l)
-                return list;
+            }
             bmin++;
         }
         return list;   
