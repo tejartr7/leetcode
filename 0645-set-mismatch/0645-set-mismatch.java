34c36fa75049e093c40f8355d39a491cb149b875
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-10-23 08:54:20 +0530

    Time: 46 ms (7.92%), Space: 60.5 MB (5.11%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,17 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.print.attribute.HashAttributeSet;
+
 public class Solution {
     public int[] findErrorNums(int[] nums) {
-        Map < Integer, Integer > map = new HashMap();
-        int dup = -1, missing = 1;
-        for (int n: nums) {
-            map.put(n, map.getOrDefault(n, 0) + 1);
+        Map<Integer,Integer> map=new HashMap<>();
+        int i,n=nums.length;
+        for(i=0;i<n;i++)
+        {
+            map.put(nums[i],map.getOrDefault(nums[i], 0)+1);
         }
-        for (int i = 1; i <= nums.length; i++) {
-            if (map.containsKey(i)) {
-                if (map.get(i) == 2)
-                    dup = i;
-            } else
-                missing = i;
+        int a[]=new int[2];
+        for(i=1;i<=n;i++)
+        {   if(map.containsKey(i))
+            {
+                if(map.get(i)==2)
+                a[0]=i;
+            }
+            else if(!map.containsKey(i))
+            a[1]=i;
         }
-        return new int[]{dup, missing};
+        return a;
+
     }
 }
