f289d3a9fcedc69d39efb7d81e04f5d6ab700de9
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-10-31 19:27:39 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (87.66%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -1,30 +1,9 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
-     int m=matrix.length,n=matrix[0].length;
-     int temp=-n+1;
-     int i,j;
-     Set<Integer> set=new HashSet<>();
-     while(temp<m)
-     {  
-        for(i=0;i<m;i++)
-        {
-            for(j=0;j<n;j++)
-            {
-                if(i-j==temp)
-                {
-                    if(set.size()==0)
-                    set.add(matrix[i][j]);
-                    else if(!set.contains(matrix[i][j]))
+        for (int r = 1; r < matrix.length; ++r)
+            for (int c = 1; c < matrix[0].length; ++c)
+                if (matrix[r-1][c-1] != matrix[r][c])
                     return false;
-                }
-            }
-        }
-        set.clear();
-        temp++;
-     }
-     return true;
+        return true;
     }
 }
