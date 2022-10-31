563bcc36c93200f2d1f8eea800931021aa784016
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-10-31 22:31:45 +0530

    Time: 101 ms (21.21%), Space: 118.6 MB (21.92%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -3,52 +3,22 @@ import java.util.Arrays;
 import java.util.*;
 
 class Solution {
+    private Double getDistance(int[] point) {
+        return Math.sqrt((point[0] * point[0]) + (point[1] * point[1]));
+    }
     public int[][] kClosest(int[][] points, int k) {
-        List<Integer> listx = new ArrayList<>();
-        List<Integer> listy = new ArrayList<>();
-        int i, j, n = points.length;
-        Map<Double, List<Integer>> map = new HashMap<>();
-        PriorityQueue<Double> pq=new PriorityQueue<>();
-        int [][]ans=new int[k][2];
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+        (p1, p2) -> getDistance(p1).compareTo(getDistance(p2)));
+        int i,n=points.length;
+        int [][]closePoints=new int[k][2];
         for(i=0;i<n;i++)
         {
-            List<Integer> dummy=new ArrayList<>();
-            double temp=(double)Math.sqrt(points[i][0]*points[i][0]+points[i][1]*points[i][1]);
-            listx.add(points[i][0]);
-            listy.add(points[i][1]);
-            pq.add(temp);
-            if(map.containsKey(temp))
-            {
-             dummy=map.get(temp);
-             dummy.add(i);
-             map.put(temp,dummy);
-             
-            }
-            else
-            {   dummy.add(i);
-                map.put(temp,dummy);
-            }
+            pq.add(points[i]);
         }
-        i=0;
-        while(k>0)
+        for(i=0;i<k;i++)
         {
-            double temp=pq.peek();
-            List<Integer> l=map.get(pq.peek());
-            pq.remove(temp);
-            if(k<=0)
-            break;
-            int size=l.size();
-            for(j=0;j<size;j++)
-            {
-                if(k<=0)
-                 break;
-                int x=l.get(j);
-                ans[i][0]=listx.get(x);
-                ans[i][1]=listy.get(x);
-                i++;
-                k--;
-            }
+            closePoints[i]=pq.poll();
         }
-        return ans;
+        return closePoints;
     }
 }
