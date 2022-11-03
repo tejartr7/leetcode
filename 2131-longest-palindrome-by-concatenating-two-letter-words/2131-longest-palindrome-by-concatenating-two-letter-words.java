c56fab5f4ca72dcb29fb0126b4df50742004e427
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-11-03 20:20:18 +0530

    Time: 15 ms (98.38%), Space: 50.8 MB (99.55%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,53 +1,31 @@
 import java.util.*;
 
 class Solution {
-    public boolean isPalan(String x) {
-        if (x.charAt(0) == x.charAt(1))
-            return true;
-        return false;
-    }
-    public String rev(String x)
-    {
-        String temp=String.valueOf(x.charAt(1))+String.valueOf(x.charAt(0));
-        return temp;
-    }
     public int longestPalindrome(String[] words) {
-        int ans=0;
-        int count=0,temp=0;
-        int i,n=words.length;
-        Map<String,Integer> map=new HashMap<>();
-        for(i=0;i<n;i++)
+       int ans=0;
+       int freq[][]=new int[26][26];
+       for(String temp:words)
+       {
+        int i=temp.charAt(0)-'a';
+        int j=temp.charAt(1)-'a';
+        if(freq[j][i]>0)
         {
-           map.put(words[i],map.getOrDefault(words[i], 0)+1);        
+            ans+=4;
+            freq[j][i]--;
         }
-        for(i=0;i<n;i++)
+        else
         {
-            if(isPalan(words[i]))
-            {
-                int x=map.get(words[i]);
-                temp+=x/2;
-                map.put(words[i],x%2);
-            }
-            else
-            {
-             if(map.containsKey(rev(words[i])))
-             {
-                int min=Math.min(map.get(rev(words[i])),map.get(words[i]));
-                map.put(words[i],map.get(words[i])-min);
-                map.put(rev(words[i]),map.get(rev(words[i]))-min);
-                temp+=min;
-             }
-            }
+            freq[i][j]++;
         }
-        temp*=4;
-        for(String x:map.keySet())
+       }
+       int i;
+       for(i=0;i<26;i++)
+       {
+        if(freq[i][i]>0)
         {
-            if(isPalan(x) && map.get(x)>0)
-            {
-                temp+=2;
-                break;
-            }
-        } 
-        return temp;
+            return ans+2;
+        }
+       }
+       return ans;
     }
 }
