85ab46a413a674bd5e2913adbf1496ac127e211d
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-11-06 11:05:43 +0530

    Time: 110 ms (40.00%), Space: 104.2 MB (20.00%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,30 +1,26 @@
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 class Solution {
-    public long maximumSubarraySum(int[] arr, int k) {
-        HashSet<Integer> set = new HashSet<>();
-		int i = 0, j = 0;
-		long sum = 0;
-		long maxSum = 0;
-		
-		while (j < arr.length) {
-			if (!set.contains(arr[j])) {               // no duplicate element in window
-                set.add(arr[j]);                       // add it to set
-                sum += arr[j];                         // add to local sum
-                if (j-i+1 == k) {                      // if window length == K
-					maxSum = Math.max(maxSum, sum);                // take the max sum
-					sum -= arr[i];                                 // remove calculation of arr[i]
-					set.remove(arr[i]);                            // remove arr[i] from set
-					i++;                                           // shift the window to the right 
-				}
-			}
-			else {                                                 // duplicate element encountered in window, reset everything
-				i = j;                                             // start window from j
-				sum = arr[i];                                      // add first element to window
-				set = new HashSet<>();                             // create a new set or clear the previous one
-				set.add(arr[j]);                                   // add current element to set
-			}
-			j++;                                                   // expand the window
-		}
-		
-		return maxSum;
+    public long maximumSubarraySum(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        int i,j,n=nums.length;
+        i=0;j=0;
+        long sum=0,max=0;
+        while(j<n)
+        {
+           while(set.size()==k|| set.contains(nums[j]))
+           {   sum-=nums[i];
+               set.remove(nums[i]);
+               i++;
+           }
+           set.add(nums[j]);
+           sum+=nums[j];
+           j++;
+           if(set.size()==k)
+           max=Math.max(sum,max);
+        }
+        return max;
     }
 }
