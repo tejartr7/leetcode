2a836b75c2ce873122f07fd5a2c3ec607b89f821
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-11-22 19:31:56 +0530

    Time: 54 ms (59.01%), Space: 42.5 MB (78.73%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,34 +1,13 @@
-class Solution {
-    public int fun(int n,int ans[])
-    {
-        int x=(int)Math.sqrt(n);
-        if(x*x==n)
-        {   ans[n]=1;
-            return 1;
-        }
-        int min=Integer.MAX_VALUE;
-        int i=1;
-        while(i<=x)
-        {
-           int val=1+ans[n-i*i];
-           i++;
-           min=Math.min(min,val);
-        }
-        return min;
-    }
+public class Solution {
     public int numSquares(int n) {
-        if(n<=3)
-            return n;
-        int i;
-         int ans[]=new int[n+1];
-         ans[0]=1;
-         ans[1]=1;
-         ans[2]=2;
-         ans[3]=3;
-        for(i=4;i<=n;i++)
-        {
-            ans[i]=fun(i,ans);
-        }
-        return ans[n];
+       int[] dp = new int[n + 1];
+       Arrays.fill(dp, Integer.MAX_VALUE);
+       dp[0] = 0;
+       for(int i = 0; i <= n; i++){
+           for(int j = 1; i + j * j <= n; j++){
+               dp[i  + j * j] = Math.min(dp[i + j * j], dp[i] + 1);
+            }
+       }
+       return dp[n];
     }
 }
