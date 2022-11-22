501c93173347aa22223c1d8bcd0c134cdae570bf
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-11-22 19:32:58 +0530

    Time: 2 ms (98.34%), Space: 40.5 MB (99.02%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,13 +1,37 @@
 public class Solution {
-    public int numSquares(int n) {
-       int[] dp = new int[n + 1];
-       Arrays.fill(dp, Integer.MAX_VALUE);
-       dp[0] = 0;
-       for(int i = 0; i <= n; i++){
-           for(int j = 1; i + j * j <= n; j++){
-               dp[i  + j * j] = Math.min(dp[i + j * j], dp[i] + 1);
-            }
-       }
-       return dp[n];
+public int numSquares(int n) {
+
+// Lagrange's Four Square theorem : every positive integer can be written as the sum of at most four squares
+       // answer to this question can be 1 or 2 or 3 or 4 only.
+	   
+	   
+        if(n == 0) return 0;
+        
+		// case 1:
+        if(isSqrt(n)) return 1;
+        
+		   // case 2 :If answer is 2 => one factor has to be (int)square_root and remaining other number has to be a perfect square
+        for(int i=1; i<=Math.sqrt(n); i++){
+            if(isSqrt(n-(i*i))) return 2;
+        }
+        
+		 // case 4: a positive integer can be expressed as the sum of three squares if and only if it is not of the form 4^k(8m+7) for some int k and m see "not of" in above line
+		 
+        // in order to pass 28 as 4
+        while(n%4 ==0)
+            n=n/4;
+        
+        if(n%8 == 7){
+            return 4;    // handling 4^k
+        }
+        
+		 // case 3
+        return 3;
+    }
+    
+    private boolean isSqrt(int n){
+        int sqrt = (int)Math.sqrt(n);
+        
+        return n-(sqrt*sqrt)==0;
     }
 }
