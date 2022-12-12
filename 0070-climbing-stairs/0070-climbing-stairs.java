ea11f6d4de5022b2bba73d4e50e25efa4cea26fa
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2022-12-12 17:57:16 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (54.50%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -5,9 +5,8 @@ class Solution {
         int a=1;
         int b=1;
         for(int i=2;i<=n;i++){
-            int temp1=a+b;
-            b=temp1;
-            a=temp1-a;
+            b=a+b;
+            a=b-a;
         }
         return b;
     }
