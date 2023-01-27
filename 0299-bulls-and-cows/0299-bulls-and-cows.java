e5f39f75983935a50bd158ddf5577c6375959b68
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-01-27 22:54:51 +0530

    Time: 5 ms (92.40%), Space: 42.1 MB (87.78%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.java
+++ 0299-bulls-and-cows/0299-bulls-and-cows.java
@@ -1,32 +1,22 @@
 class Solution {
     public String getHint(String secret, String guess) {
-        String ans="";
-        int common=0;
-        int i;
-        int a[]=new int[10];
-        int b[]=new int[10];
-        for(i=0;i<secret.length();i++)
-        {
-            int c=secret.charAt(i)-'0';
-            a[c]++;
-        }
-        for(i=0;i<secret.length();i++)
-        {
-            int c=guess.charAt(i)-'0';
-            b[c]++;
-        }
-        for(i=0;i<10;i++)
-        {
-            common+=Math.min(a[i],b[i]);
-        }
-      //  System.out.println(common);
-        int y=0;
-        for(i=0;i<secret.length();i++)
+       //Time complexity=O(n+n)~O(n)
+        //Space complexity=O(10+2)~O(1)
+       int x=0,y=0;
+       int a[]=new int[10];
+       int i,n=secret.length();
+        for(i=0;i<n;i++)
         {
             if(secret.charAt(i)==guess.charAt(i))
-                y++;
+                x++;
+            a[secret.charAt(i)-'0']++;
         }
-        ans=y+"A"+(common-y)+"B";
-        return ans;
-    }
+        for(i=0;i<n;i++)
+        {
+            if(a[guess.charAt(i)-'0']>0)
+                y++;
+            a[guess.charAt(i)-'0']--;
+        }
+        return x+"A"+(y-x)+"B";
+    }   
 }
