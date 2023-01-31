dea84d494686682e7e1bcb8032d12fe2bdbf2c4f
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-01-31 18:40:33 +0530

    Time: 1 ms (100.00%), Space: 44.1 MB (90.37%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.java
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.java
@@ -1,44 +1,11 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
-    int sum=0;
-    public void grandChild(TreeNode root,int level)
-    {
-        if(root==null)
-            return ;
-        if(level==2)
-        {
-            sum+=root.val;
-        }
-        grandChild(root.left,level+1);
-        grandChild(root.right,level+1);
+     public int sumEvenGrandparent(TreeNode root) {
+        return helper(root, 1, 1);
     }
-    public void preorder(TreeNode root)
-    {
-        if(root==null)
-            return ;
-        if(root.val%2==0)
-        {
-            grandChild(root,0);
-        }
-        preorder(root.left);
-        preorder(root.right);
-    }
-    public int sumEvenGrandparent(TreeNode root) {
-        preorder(root);
-        return sum;    
+
+    public int helper(TreeNode node, int p, int gp) {
+        if (node == null) return 0;
+        return helper(node.left, node.val, p) + helper(node.right, node.val, p) + (gp % 2 == 0 ? node.val : 0);
     }
 }
