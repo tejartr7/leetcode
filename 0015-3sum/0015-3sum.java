d7a17ccad6788ea3ff94ebcbe4185e7bef7a9eba
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-02-01 23:34:33 +0530

    Time: 39 ms (56.69%), Space: 49.7 MB (45.81%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+     Arrays.sort(nums);
+        int i,l=nums.length;
+        List<List<Integer>> res=new ArrayList<>();
+        for(i=0;i<l;i++)
+        {
+            int j=i+1;
+            int k=l-1;
+            if(i>0 && nums[i]==nums[i-1])
+                continue;
+            while(j<k)
+            {
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum==0)
+                {   List<Integer> a=new ArrayList<>();
+                    a.add(nums[i]);
+                    a.add(nums[j]);
+                    a.add(nums[k]);
+                    res.add(a);
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1])
+                    j++;
+                 }
+                else if(sum<0) j++;
+                else
+                    k--;
+                    
+            }
+          
+        }
+          return res;
+    }
+}
