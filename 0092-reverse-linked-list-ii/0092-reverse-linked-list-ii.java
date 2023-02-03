d06cfb55864a87b7722eb27ee3cd56a60f1ebbd9
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-02-03 21:40:00 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (98.72%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -1,43 +1,38 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        int count = 0;
-        ListNode curr = head;
-        while (curr != null) {
-            curr = curr.next;
-            count++;
+        if(left == right){
+            return head;
         }
-        int arr[] = new int[count];
-        int i = 0;
-        curr = head;
-        while (curr != null) {
-            arr[i++] = curr.val;
-            curr = curr.next;
+
+        ListNode current = head;
+        ListNode prev = null;
+        // To skip till left we iterate till where we want our          reverse our linkedList
+        for(int i =0;current != null && i<left-1;i++){
+            prev = current;
+            current = current.next;
         }
-        left--;
-        right--;
-        while (left < right) {
-            int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-            right--;
-            left++;
+
+        ListNode last = prev;  // store this values which helps to point at the end which is 1 
+        ListNode newEnd = current; // newEnd = 2
+
+        // reverse the linkedList 
+        ListNode next = current.next;
+        for(int i =0;current != null && i<right - left +1;i++){
+            current.next = prev;
+            prev = current;
+            current = next;
+            if(next != null){
+                next = next.next;
+            }
         }
-        curr = head;
-        i = 0;
-        while (curr != null) {
-            curr.val = arr[i++];
-            curr = curr.next;
+
+        if(last!= null){
+            last.next = prev;  // point last to prev which is 4 at this poiny
+        }else{
+            head = prev;
         }
+
+        newEnd.next = current;  // point newEnd to current which is 5 at this point.
         return head;
     }
 }
