4db9083debd129bd7dcb44363fb6a36e2d2c6ac0
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-02-09 20:29:10 +0530

    Time: 2562 ms (5.02%), Space: 39.1 MB (60.75%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,13 +1,13 @@
 class Solution {
-    public int numTrees(int n) {
-  int [] G = new int[n+1];
-  G[0] = G[1] = 1;
-    
-  for(int i=2; i<=n; ++i) {
-    for(int j=1; j<=i; ++j) {
-      G[i] += G[j-1] * G[i-j];
+ public int numTrees(int n) {
+        return trees(1, n);
     }
-  }
-  return G[n];
-}
+
+    int trees(int lo, int hi) {
+        if (lo >= hi) return 1;
+        int total = 0;
+        for (int i = lo; i <= hi; i++)
+            total += trees(lo, i - 1) * trees(i + 1, hi);
+        return total;
+    }
 }
