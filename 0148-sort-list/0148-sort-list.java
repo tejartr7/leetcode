52923725769bf1dfc75eb37edc4903b837b7e903
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-02-11 15:25:23 +0530

    Time: 14 ms (48.10%), Space: 51.2 MB (47.90%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,66 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode getMid(ListNode head) {
+        ListNode slow = head, fast = head;
+        ListNode prev=null;
+        while (fast != null && fast.next != null) {
+            prev=slow;
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if(prev!=null)
+        prev.next=null;
+        return slow;
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode a = new ListNode(-1);
+        ListNode curr = a;
+        if(list1==null)
+            return list2;
+        if(list2==null)
+            return list1;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+        while (list1 != null) {
+            curr.next = list1;
+            list1 = list1.next;
+            curr = curr.next;
+        }
+        while (list2 != null) {
+            curr.next = list2;
+            list2 = list2.next;
+            curr = curr.next;
+        }
+        return a.next;
+    }
+
+    public ListNode sortList(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode mid = getMid(head);
+        ListNode right = mid.next;
+        ListNode left = head;
+        left = sortList(left);
+        right = sortList(mid);
+        ListNode result = mergeTwoLists(left, right);
+        return result;
+    }
+}
