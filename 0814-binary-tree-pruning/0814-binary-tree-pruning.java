67adeb5a79819879873d48697021a1f9012447e7
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-02-12 11:39:12 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (92.20%) - LeetHub

--- 0814-binary-tree-pruning/0814-binary-tree-pruning.java
+++ 0814-binary-tree-pruning/0814-binary-tree-pruning.java
@@ -14,25 +14,11 @@
  * }
  */
 class Solution {
-    public boolean contains1(TreeNode root)
-    {
-        if(root==null) return false;
-        if(root.val==0 && root.left==null && root.right==null)
-            return false;
-        if(root.val==1)
-            return true;
-        return contains1(root.left)||contains1(root.right);
-    }
     public TreeNode pruneTree(TreeNode root) {
         if(root==null) return root;
-        boolean left=contains1(root.left);
-        boolean right=contains1(root.right);
-        //System.out.println(left+" "+right);
-        if(!left) root.left=null;
-        if(!right)root.right=null;
-        if(root.val==0 && left==false && right==false) return null;
-        pruneTree(root.left);
-        pruneTree(root.right);
+        root.left=pruneTree(root.left);
+        root.right=pruneTree(root.right);
+        if(root.val==0 && root.left==null && root.right==null) return null;
         return root;
     }
 }
