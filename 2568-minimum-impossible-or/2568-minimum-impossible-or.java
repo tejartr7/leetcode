03620b1282ab1352c4add122d1c21914cb82f989
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-02-18 22:00:46 +0530

    Time: 26 ms (66.67%), Space: 54.9 MB (50.00%) - LeetHub

---
+++ 2568-minimum-impossible-or/2568-minimum-impossible-or.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minImpossibleOR(int[] nums) {
+        Set<Integer> set=new HashSet<>();
+        for(int x:nums) set.add(x);
+        int ans=1;
+        while(set.contains(ans))
+        {
+            ans=(ans<<1);
+        }
+        return ans;
+    }
+}
