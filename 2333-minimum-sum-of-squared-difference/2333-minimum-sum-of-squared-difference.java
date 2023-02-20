4f35b63f54977b5a17d381594359e8eb3f39c88f
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-02-20 20:17:07 +0530

    Time: 243 ms (8.27%), Space: 56.2 MB (94.22%) - LeetHub

--- 2333-minimum-sum-of-squared-difference/2333-minimum-sum-of-squared-difference.java
+++ 2333-minimum-sum-of-squared-difference/2333-minimum-sum-of-squared-difference.java
@@ -1,32 +1,30 @@
 class Solution {
     public long minSumSquareDiff(int[] nums1, int[] nums2, int k1, int k2) {
         long ans = 0;
-        int diff[] = new int[(int) 1e5 + 1];
-        Arrays.fill(diff, 0);
-        int i, n = nums1.length, k = k1 + k2;
-        for (i = 0; i < n; i++) {
-            diff[Math.abs(nums1[i] - nums2[i])]++;
+        TreeMap<Integer,Integer> map=new TreeMap<>();
+        int i,n=nums1.length;
+        for(i=0;i<n;i++)
+        {
+            int diff=Math.abs(nums1[i]-nums2[i]);
+            map.put(diff,map.getOrDefault(diff,0)+1);
         }
-        for (i = 100000; i >= 1; i--) {
-            int curr = diff[i];
-            if(diff[i]<=0)
-                continue;
-            else if (curr <= k) {
-                diff[i] = 0;
-                diff[i - 1] += curr;
-                k-=curr;
-            } else {
-                diff[i] -= k;
-                diff[i - 1] += k;
-                break;
-            }
+        int k=k1+k2;
+        while(k>0)
+        {
+            int largest=map.lastKey();
+            if(largest==0) return 0;
+            int mini=Math.min(k,map.get(largest));
+            map.put(largest-1,map.getOrDefault(largest-1,0)+mini);
+            k-=mini;
+            map.put(largest,map.get(largest)-mini);
+            if(map.get(largest)==0)
+                map.remove(largest);
         }
-        for (i = 1; i <= 100000; i++) {
-            long x=(long)i;
-            long y=(long)diff[i];
-            long temp= (x*x);
-            temp*=y;
-            ans+=temp;
+        for(int key:map.keySet())
+        {
+            long x=key;
+            long y=map.get(key);
+            ans+=x*x*y;
         }
         return ans;
     }
