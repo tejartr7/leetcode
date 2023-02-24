e661797cbe97c114dbf227a3fe32882de10a98d1
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-02-24 23:30:39 +0530

    Time: 96 ms (9.70%), Space: 57.4 MB (35.15%) - LeetHub

--- 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.java
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.java
@@ -7,7 +7,7 @@ class Solution {
     public int largestCombination(int[] c) {
         int maxi=0;
         int i,j,n=c.length;
-        for(i=0;i<32;i++)
+        for(i=0;i<30;i++)
         {   int count=0;
             for(j=0;j<n;j++)
             {
