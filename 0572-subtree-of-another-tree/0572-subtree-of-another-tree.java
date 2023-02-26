56bc95ab8482ccad75789439b96184deddb48f39
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-02-26 10:17:38 +0530

    Time: 3 ms (90.40%), Space: 42.2 MB (67.97%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -1,50 +1,17 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public List<Integer> inorder(TreeNode root,List<Integer> list)
-    {
-        if(root==null)
-            return list;
-        inorder(root.left,list);
-        list.add(root.val);
-        inorder(root.right,list);
-        return list;
-    }
-    public List<Integer> preorder(TreeNode root,List<Integer> list)
-    {
-        if(root==null)
-            return list;
-        list.add(root.val);
-        inorder(root.left,list);
-        inorder(root.right,list);
-        return list;
-    }
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(root==null && subRoot!=null) return false;
-        if(root==null && subRoot==null) return true;
-        List<Integer> list=new ArrayList<>();
-        list=inorder(subRoot,list);
-        List<Integer> pre=new ArrayList<>();
-        pre=preorder(subRoot,pre);
-        List<Integer> list1=new ArrayList<>();
-        list1=inorder(root,list1);
-        List<Integer> pre1=new ArrayList<>();
-        pre1=preorder(root,pre1);
-        if(list1.equals(list) && pre1.equals(pre))
+        if (root == null) {
+            return false;
+        }
+        if (isIdentical(root, subRoot)) {
             return true;
-        return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
+        }
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+    private boolean isIdentical(TreeNode node1, TreeNode node2) {
+        if (node1 == null || node2 == null) {
+            return node1 == null && node2 == null;
+        }
+        return node1.val == node2.val && isIdentical(node1.left, node2.left) && isIdentical(node1.right, node2.right);
     }
 }
