8291488e1224d9ed07612e41716dc302874daf17
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-02-28 18:47:51 +0530

    Time: 10 ms (96.44%), Space: 43.3 MB (90.17%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.java
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.java
@@ -7,19 +7,19 @@ class Solution {
 
     public int traverse(TreeNode node, Map<String, Integer> tripletToID,
             Map<Integer, Integer> cnt, List<TreeNode> res) {
-        if (node == null) {
-            return 0;
+        if(node==null) return 0;
+        String temp=traverse(node.left,tripletToID,cnt,res)+","+node.val+","+
+        traverse(node.right,tripletToID,cnt,res);
+        if(!tripletToID.containsKey(temp))
+        {
+            tripletToID.put(temp,tripletToID.size()+1);
         }
-        String triplet = traverse(node.left, tripletToID, cnt, res) + "," + node.val +
-                "," + traverse(node.right, tripletToID, cnt, res);
-        if (!tripletToID.containsKey(triplet)) {
-            tripletToID.put(triplet, tripletToID.size() + 1);
-        }
-        int id = tripletToID.get(triplet);
-        cnt.put(id, cnt.getOrDefault(id, 0) + 1);
-        if (cnt.get(id) == 2) {
+        int x=tripletToID.get(temp);
+        cnt.put(x,cnt.getOrDefault(x,0)+1);
+        if(cnt.get(x)==2)
+        {
             res.add(node);
         }
-        return id;
+        return x;
     }
 }
