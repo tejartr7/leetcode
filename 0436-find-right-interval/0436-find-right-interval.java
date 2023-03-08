60515c3c734872ff3ab26ce45e8d6ef361526466
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-03-08 23:35:43 +0530

    Time: 32 ms (32.92%), Space: 50.7 MB (16.54%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.java
+++ 0436-find-right-interval/0436-find-right-interval.java
@@ -1,23 +1,21 @@
 class Solution {
     public int[] findRightInterval(int[][] intervals) {
-        int n = intervals.length;
-        int start[][]=new int[n][2];
-        for(int i=0;i<n;i++){
-            start[i][0] = intervals[i][0];
-            start[i][1] = i;
+        TreeMap<Integer,Integer> map=new TreeMap<>();
+        int i,n=intervals.length;
+        int ans[]=new int[n];
+        Arrays.fill(ans,-1);
+        for(i=0;i<n;i++)
+        {
+            map.put(intervals[i][0],i);
         }
-        Arrays.sort(start, (a,b) -> (a[0] - b[0]));
-        int ans[] = new int[n];
-        for(int i=0;i<n;i++) {
-            int target = intervals[i][1];
-            int l = 0, r = n-1;
-            while(l<=r) {
-                int mid = (l+r)/2;
-                if(start[mid][0] >= target) r = mid - 1;
-                else if(start[mid][0] < target) l = mid + 1;   
-            }
-            if(l<n) ans[i] = start[l][1];
-            else ans[i] = -1;
+       // System.out.println(map);
+        for(i=0;i<n;i++)
+        {
+            int x=intervals[i][1];
+            int y=map.ceilingKey(x)!=null?map.ceilingKey(x):Integer.MIN_VALUE;
+           // System.out.println(y);
+            if(y!=Integer.MIN_VALUE)
+                ans[i]=map.get(y);
         }
         return ans;
     }
