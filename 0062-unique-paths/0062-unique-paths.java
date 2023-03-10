0f2851ebb1fb2379d786c9a4ee950da39e9dbebc
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-03-10 20:12:52 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (23.01%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int arr[][]=new int[m][n];
+        int i,j;
+        if(m==1 && n==1) return 1;
+        if(m==2 && n==1) return 1;
+        if(m==1 && n==2) return 1;
+        j=n-1;
+        for(i=0;i<m;i++)
+            arr[i][j]=1;
+        i=m-1;
+        for(j=0;j<n;j++)
+            arr[i][j]=1;
+        for(i=m-2;i>=0;i--)
+        {
+            for(j=n-2;j>=0;j--)
+            {
+                arr[i][j]=arr[i+1][j]+arr[i][j+1];
+            }
+        }
+        return arr[0][0];
+    }
+}
