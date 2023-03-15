649a4a24fbea0041db72bc2a36b4b2450825a8ef
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-03-15 09:07:23 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (5.73%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
@@ -1,51 +1,23 @@
 class Solution {
+    int count=0;
+    public int countNodes(TreeNode root) {
+        if(root==null)
+        {
+            return 0;
+        }
+        count=1+countNodes(root.left)+countNodes(root.right);
+        return count;
+    }
+    public boolean dfs(TreeNode root,int index,int n)
+    {
+        if(root==null) return true;
+        //because in complete binary tree child notes will be at 2i+1 and 2i+2
+        // if they are not at those place that means it is not a complete binary tree
+        if(index>=n)   return false;
+        return dfs(root.left,2*index+1,n) && dfs(root.right,2*index+2,n);
+    }
     public boolean isCompleteTree(TreeNode root) {
-        Queue<TreeNode> q=new LinkedList<>();
-        List<List<Integer>> list=new ArrayList<>();
-        q.offer(root);
-        while(!q.isEmpty())
-        {
-            int i,n=q.size();
-            List<Integer> sublist=new ArrayList<>();
-            for(i=0;i<n;i++)
-            {
-                TreeNode top=q.poll();
-                if(top.val==-1)
-                {
-                    sublist.add(-1);
-                    continue;
-                }
-                if(top.left!=null)
-                    q.offer(top.left);
-                if(top.left==null)
-                    q.offer(new TreeNode(-1));
-                if(top.right!=null)
-                    q.offer(top.right);
-                if(top.right==null)
-                    q.offer(new TreeNode(-1));
-                if(top.val!=-1)
-                    sublist.add(top.val);
-            }
-            list.add(sublist);
-        }
-        //System.out.println(list);
-        int i;
-        for(i=0;i<list.size()-1;i++)
-        {
-            boolean found=false;
-            boolean pos=false;
-            for(int x:list.get(i))
-            {
-                if(x==-1 && i!=list.size()-2)
-                    return false;
-                if(x==-1 && !found)
-                {
-                    found=true;
-                }
-                else if(found && x>0) 
-                    return false;
-            }
-        }
-        return true;
+       countNodes(root);
+       return dfs(root,0,count);
     }
 }
