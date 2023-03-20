76ae8365f73d6981b1bf38a883a9427ac85789bd
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-03-20 20:08:16 +0530

    Time: 211 ms (83.33%), Space: 60.1 MB (50.00%) - LeetHub

--- 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.java
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.java
@@ -1,40 +1,33 @@
+class Pair{
+    int num;
+    int index;
+    Pair(int num, int index)
+    { this.num = num; this.index = index; }
+}
 class Solution {
-    public long findScore(int[] nums) {
-        long sum=0;
-        int i,n=nums.length;
-        TreeMap<Integer,TreeSet<Integer>> map=new TreeMap<>();
-        for(i=0;i<n;i++)
+    public long findScore(int[] nums) 
+    {
+        int n = nums.length;
+        PriorityQueue<Pair>pq = new PriorityQueue<Pair>((Pair a, Pair b)->{
+            if (a.num != b.num) return (a.num - b.num);
+            else return (a.index - b.index);
+        });
+        for (int i = 0; i < nums.length; i++ ) pq.add(new Pair(nums[i], i));
+        boolean[] marked = new boolean[n];
+        
+        long ans = 0;
+        while(!pq.isEmpty())
         {
-            if(!map.containsKey(nums[i]))
-                map.put(nums[i],new TreeSet<>());
-            map.get(nums[i]).add(i);
+            int currNum = pq.peek().num;
+            int currIdx = pq.peek().index;
+            pq.poll();
+            if (marked[currIdx]) continue;
+            
+            ans += currNum;
+            marked[currIdx] = true;
+            if (currIdx + 1 < n) marked[currIdx + 1] = true;
+            if (currIdx - 1 >= 0) marked[currIdx - 1] = true; 
         }
-        while(map.size()>0)
-        {
-            int x=map.firstKey();
-            sum+=x;
-            int ind=map.get(x).first();
-            map.get(x).remove(ind);
-            if(map.get(x).size()==0)
-                map.remove(x);
-            if(ind-1>=0)
-            {
-                //int temp=map.get(nums[ind]-1).first();
-                if(map.containsKey(nums[ind-1])){
-                map.get(nums[ind-1]).remove(ind-1);
-                if(map.get(nums[ind-1]).size()==0)
-                    map.remove(nums[ind-1]);
-                }
-            }
-            if(ind+1<n)
-            {
-                if(map.containsKey(nums[ind+1])){
-                map.get(nums[ind+1]).remove(ind+1);
-                if(map.get(nums[ind+1]).size()==0)
-                    map.remove(nums[ind+1]);
-                }
-            }
-        }
-        return sum;
+        return ans;
     }
 }
