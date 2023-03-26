99e999199d84e3c5e365be7e4380fc0f8532e494
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-03-26 11:56:08 +0530

    Time: 4 ms (82.50%), Space: 43.5 MB (29.58%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.java
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.java
@@ -2,21 +2,12 @@ class Solution {
     public int minMoves2(int[] nums) {
         int i,n=nums.length;
         Arrays.sort(nums);
-        long pre[]=new long[n];
-        long suff[]=new long[n];
-        pre[0]=suff[n-1]=0;
-        for(i=1;i<n;i++)
-            pre[i]=pre[i-1]+nums[i-1];
-        for(i=n-2;i>=0;i--)
-            suff[i]=suff[i+1]+nums[i+1];
-        long ans=Long.MAX_VALUE;
-        for(i=0;i<n;i++)
+        int median=nums[n/2];
+        int ans=0;
+        for(int x:nums)
         {
-            long sum=Math.abs(pre[i]-(long)i*(long)nums[i]);
-            sum+=Math.abs(suff[i]-(long)(n-1-i)*(long)nums[i]);
-            //System.out.print(sum+" ");
-            ans=Math.min(ans,sum);
+            ans+=Math.abs(x-median);
         }
-        return (int)ans;
+        return ans;
     }
 }
