b1a097b14c216b6a1b8ac980a63b445b90227ca2
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-03-30 15:39:05 +0530

    Time: 6 ms (59.69%), Space: 43.4 MB (48.62%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,21 +1,61 @@
-class Solution {
-       public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K)
+class pair
+{
+    int node;
+    int weight;
+    pair(int node,int weight)
     {
-        int[] cost=new int[n];
-        Arrays.fill(cost,Integer.MAX_VALUE);
-        cost[src]=0;
-        for(int i=0;i<=K;i++)
+        this.node=node;
+        this.weight=weight;
+    }
+}
+class tuple{
+    int stops;
+    int node;
+    int dis;
+    tuple(int x,int y,int z)
+    {
+        stops=x;
+        node=y;
+        dis=z;
+    }
+}
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        Map<Integer,List<pair>> map=new HashMap<>();
+        int i;
+        int l=flights.length;
+        for(i=0;i<n;i++)
         {
-            int[] temp= Arrays.copyOf(cost,n);
-            for(int[] f: flights)
-            {
-                int curr=f[0],next=f[1],price=f[2];
-                if(cost[curr]==Integer.MAX_VALUE)
-                    continue;
-                temp[next]=Math.min(temp[next],cost[curr]+price);
-            }
-            cost=temp;
+            map.put(i,new ArrayList<>());
         }
-        return cost[dst]==Integer.MAX_VALUE?-1:cost[dst];
+        for(i=0;i<l;i++)
+        {
+            map.get(flights[i][0]).add(new pair(flights[i][1],flights[i][2]));
+        }
+        int dist[]=new int[n];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[src]=0;
+        Queue<tuple>q =new LinkedList<>();
+        q.offer(new tuple(0,src,0));
+        while(!q.isEmpty())
+        {
+            tuple top=q.poll();
+            int stop=top.stops;
+            int node=top.node;
+            int dis=top.dis;
+            if(stop>k)
+                continue;
+            for(pair x:map.get(node))
+            {
+                int a=x.node;
+                int b=x.weight;
+                if(b+dis<dist[a]&& stop<=k)
+                {
+                    dist[a]=b+dis;
+                    q.offer(new tuple(stop+1,a,b+dis));
+                }
+            }
+        }
+        return dist[dst]==Integer.MAX_VALUE?-1:dist[dst];
     }
 }
