2b2a5bf3a7cd7a733bb5d82a7f9a1feb2845b29a
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-03-30 18:25:45 +0530

    Time: 22 ms (90.42%), Space: 49.2 MB (43.71%) - LeetHub

--- 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.java
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.java
@@ -1,26 +1,26 @@
 class Solution {
-    public int longestBeautifulSubstring(String word) {
-        int maxi=0;
-        int count=1;
+  public int longestBeautifulSubstring(String word) {
+        int cnt=1;
         int len=1;
-        int i,n=word.length();
-        for(i=1;i<n;i++)
-        {
-            if(word.charAt(i)>=word.charAt(i-1))
-            {
+        int max_length=0;
+        
+        int n=word.length();
+        
+        for(int i=1;i<n;i++){
+            if(word.charAt(i)==word.charAt(i-1)){
+                 len++;
+            }else if(word.charAt(i-1)<word.charAt(i)){
+                cnt++;
                 len++;
+            }else{
+                len=1;
+                cnt=1;
             }
-            if(word.charAt(i)>word.charAt(i-1))
-                count++;
-            if(word.charAt(i)<word.charAt(i-1))
-            {
-                len=count=1;
-            }
-            if(count==5)
-            {
-                maxi=Math.max(maxi,len);
+            
+            if(cnt==5){
+                max_length=Math.max(max_length,len);
             }
         }
-        return maxi;
+        return max_length;
     }
 }
