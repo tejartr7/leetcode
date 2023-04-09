1c35c7f0df6cf4f9c835a79a5138274d1127bc84
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-04-09 14:28:04 +0530

    Time: 10 ms (43.30%), Space: 43.3 MB (21.26%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -1,14 +1,22 @@
 class Solution {
     public int[] sortByBits(int[] arr) {
-        TreeMap<Integer,TreeSet<Integer>> map=new TreeMap<>();
+        TreeMap<Integer,List<Integer>> map=new TreeMap<>();
         int i,n=arr.length;
         for(i=0;i<n;i++)
         {
-            arr[i]+=Integer.bitCount(arr[i])*10001;
+            int x=Integer.bitCount(arr[i]);
+            if(!map.containsKey(x))
+            map.put(x,new ArrayList<>());
+            map.get(x).add(arr[i]);
         }
-        Arrays.sort(arr);
-        for(i=0;i<n;i++)
-            arr[i]%=10001;
+        i=0;
+        for(int x:map.keySet())
+        {
+            List<Integer> list=new ArrayList<>(map.get(x));
+            Collections.sort(list);
+            for(int y:list)
+                arr[i++]=y;
+        }
         return arr;
     }
 }
