210f4cbd2a6a0bc2aaaac28e1e133f7394f696e4
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-04-22 23:15:27 +0530

    Time: 37 ms (59.01%), Space: 44.1 MB (26.99%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,28 +1,40 @@
 class Solution {
-    private int lcs(String s1, String s2, int m, int n, int[][] memo) {
-        if (m == 0 || n == 0) {
-            return 0;
+    public int longestCommonSubsequence(String text1, String text2) {
+        int i,j,n1=text1.length(),n2=text2.length();
+        int dp[][]=new int[n1+1][n2+1];
+        for(i=0;i<n1;i++)
+            dp[i][0]=0;
+        for(j=0;j<n2;j++)
+        {
+           dp[0][j]=0;
         }
-        if (memo[m][n] != -1) {
-            return memo[m][n];
-        }
-        if (s1.charAt(m - 1) == s2.charAt(n - 1)) {
-            return memo[m][n] = 1 + lcs(s1, s2, m - 1, n - 1, memo);
-        }
-        return memo[m][n] = Math.max(lcs(s1, s2, m - 1, n, memo), lcs(s1, s2, m, n - 1, memo));
-    }
-
-    public int minInsertions(String s) {
-        int n = s.length();
-        String sReverse = new StringBuilder(s).reverse().toString();
-        int[][] memo = new int[n + 1][n + 1];
-
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= n; j++) {
-                memo[i][j] = -1;
+        for(i=1;i<=n1;i++)
+        {
+            for(j=1;j<=n2;j++)
+            {
+                if(text1.charAt(i-1)!=text2.charAt(j-1))
+                {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+                else
+                {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
             }
         }
-
-        return n - lcs(s, sReverse, n, n, memo);
+        return dp[n1][n2];
+    }
+    public int minInsertions(String s) {
+        char ch[]=s.toCharArray();
+        int i=0,j=ch.length-1;
+        while(i<j)
+        {
+            char temp=ch[i];
+            ch[i]=ch[j];
+            ch[j]=temp;
+            i++;
+            j--;
+        }
+        return s.length()-longestCommonSubsequence(s,new String(ch));
     }
 }
