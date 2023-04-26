9bbf01cacf07c1cc10448cf79c4f72115965c428
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-04-26 13:19:07 +0530

    Time: 1 ms (100.00%), Space: 40 MB (43.76%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int addDigits(int num) {
+        int k;
+        int r;
+        if(num==0)
+        return 0;
+        if(num%9==0)
+            return 9;
+        else
+            return num%9;
+    }
+}
