62c236ece35a2e131402746778e8a7a21e8a0557
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-05-17 11:21:45 +0530

    Time: 51 ms (96.64%), Space: 42.3 MB (31.03%) - LeetHub

---
+++ 2636-promise-pool/2636-promise-pool.js
@@ -0,0 +1,23 @@
+var promisePool = async function(functions, n) {
+    return new Promise((resolve) => {
+        let inProgressCount = 0;
+        let functionIndex = 0;
+        function helper() {
+            if (functionIndex >= functions.length) {
+                if (inProgressCount === 0) resolve();
+                return;
+            }
+
+            while (inProgressCount < n && functionIndex < functions.length) {
+                inProgressCount++;
+                const promise = functions[functionIndex]();
+                functionIndex++;
+                promise.then(() => {
+                    inProgressCount--;
+                    helper();
+                });
+            }
+        }
+        helper();
+    });
+};
