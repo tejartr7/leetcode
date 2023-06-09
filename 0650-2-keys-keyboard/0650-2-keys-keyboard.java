7b7b206e5f1664a914dc8b5a75d361d259226b49
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-06-09 21:14:19 +0530

    Time: 4 ms (61.07%), Space: 39.6 MB (58.89%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -1,50 +1,12 @@
 class Solution {
     int mini=Integer.MAX_VALUE;
-    public boolean isPrime(int n)
-    {
-        if (n <= 1)
-            return false;
- 
-        // Check if n=2 or n=3
-        if (n == 2 || n == 3)
-            return true;
- 
-        // Check whether n is divisible by 2 or 3
-        if (n % 2 == 0 || n % 3 == 0)
-            return false;
- 
-        // Check from 5 to square root of n
-        // Iterate i by (i+6)
-        for (int i = 5; i <= Math.sqrt(n); i = i + 6)
-            if (n % i == 0 || n % (i + 2) == 0)
-                return false;
- 
-        return true;
-    }
-    public void helper(int curr,int copy,int steps,int n)
-    {
-        if(curr==n)
-        {
-            mini=Math.min(mini,steps);
-            return ;
-        }
-        
-    }
     public int minSteps(int n) {
         //helper(1,0,0,n);
-        if(isPrime(n))
-            return n;
         int dp[]=new int[n+1];
         Arrays.fill(dp,1000000);
         int i;
-        for(i=0;i<=Math.min(5,n);i++)
-        {
-            dp[i]=i;
-        }
-        dp[1]=0;
-        if(n<=5)
-            return dp[n];
-        for(i=6;i<=n;i++)
+        dp[0]=dp[1]=0;
+        for(i=2;i<=n;i++)
         {
             int x,y;
             for(x=2;x*x<=i;x++)
@@ -54,8 +16,10 @@ class Solution {
                     dp[i]=Math.min(dp[i],dp[i/x]+dp[x]);
                 }
             }
-            if(isPrime(i))
+            if(dp[i]==1000000)
+            {//this mean that the number is a prime number
                 dp[i]=i;
+            }
         }
         return dp[n];
     }
