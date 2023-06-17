d30c65a5de8c70de50aab49af4b0ddf13978d1d2
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-06-17 21:42:38 +0530

    Time: 93 ms (15.31%), Space: 74.9 MB (23.92%) - LeetHub

---
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
@@ -0,0 +1,30 @@
+class Solution {
+    long[][] dp;
+
+    public long helper(int i, int x, int[] nums) {
+        int n = nums.length;
+        if (i >= n) {
+            return 0;
+        }
+        if (dp[i][x] != -1) {
+            return dp[i][x];
+        }
+        if(x==1)
+        {
+            long a=helper(i+1,0,nums)+nums[i];
+            long b=helper(i+1,1,nums);
+            return dp[i][x]=Math.max(a,b);
+        }
+        long a=helper(i+1,1,nums)-nums[i];
+        long b=helper(i+1,0,nums);
+        return dp[i][x]=Math.max(a,b);
+    }
+
+    public long maxAlternatingSum(int[] nums) {
+        int n = nums.length;
+        dp = new long[n][2];
+        for(int i=0;i<nums.length;i++)
+            Arrays.fill(dp[i],-1);
+        return helper(0, 1, nums);
+    }
+}
