8b67c4d8b8137283f8b9b5c29570b362c8b0eae0
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-06-18 14:01:53 +0530

    Time: 1882 ms (57.34%), Space: 0B (100.00%) - LeetHub

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,5 @@
+SELECT contest_id
+    , ROUND(COUNT(DISTINCT user_id) * 100 / (SELECT COUNT(*) FROM Users), 2) AS percentage
+FROM Register 
+GROUP BY contest_id
+    ORDER BY percentage DESC, contest_id
