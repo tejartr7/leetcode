339f6426291c90bc55fa65724b6cc66665526f2a
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-06-19 11:18:00 +0530

    Time: 166 ms (23.20%), Space: 138.9 MB (6.77%) - LeetHub

---
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
@@ -0,0 +1,37 @@
+class Solution {
+    int[] disc, low;
+    int time = 1;
+    List<List<Integer>> ans = new ArrayList<>();
+    Map<Integer, List<Integer>> edgeMap = new HashMap<>();
+
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+        disc = new int[n];
+        low = new int[n];
+        for (int i = 0; i < n; i++) {
+            edgeMap.put(i, new ArrayList<Integer>());
+        }
+        for (List<Integer> conn : connections) {
+            int a = conn.get(0);
+            int b = conn.get(1);
+            edgeMap.get(a).add(b);
+            edgeMap.get(b).add(a);
+        }
+        dfs(0, -1);
+        return ans;
+    }
+
+    public void dfs(int curr, int prev) {
+        disc[curr] = low[curr] = time++;
+        for (int next : edgeMap.get(curr)) {
+            if (disc[next] == 0) {
+                dfs(next, curr);
+                low[curr] = Math.min(low[curr], low[next]);
+                if (low[next] > disc[curr]) {
+                    ans.add(Arrays.asList(curr, next));
+                }
+            } else if (next != prev) {
+                low[curr] = Math.min(low[curr], disc[next]);
+            }
+        }
+    }
+}
