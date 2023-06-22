468c9b7ceca3a6c55b87d1cd08f28bec4750c662
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-06-22 16:18:42 +0530

    Time: 579 ms (15.11%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,11 +1,6 @@
-SELECT NULLIF(
-    (SELECT DISTINCT salary
-     FROM Employee s1
-     WHERE 1 = (
-         SELECT COUNT(DISTINCT salary)
-         FROM Employee s2
-         WHERE s2.salary > s1.salary
-     )
-    ),
-    NULL
-) AS SecondHighestSalary;
+SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary < (
+    SELECT MAX(salary)
+    FROM Employee
+)
