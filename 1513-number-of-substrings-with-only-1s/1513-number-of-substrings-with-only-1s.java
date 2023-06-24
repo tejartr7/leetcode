7495e24fdb136a3506de479971114e418913fe52
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-06-24 14:45:48 +0530

    Time: 7 ms (69.86%), Space: 43.8 MB (37.80%) - LeetHub

--- 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.java
+++ 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.java
@@ -3,25 +3,15 @@ class Solution {
         int n=s.length();
         int res=0;
         int i=0,j=0;
-        int l=0;
+        int cnt=0;
         int mod=1000_000_007;
-        while(j<n)
+        for(i=0;i<n;i++)
         {
-            
-            if(s.charAt(j)=='0')
-            {
-                i=j;
-            }
-            while(i<n && s.charAt(i)=='0')
-            {
-                i++;
-                l++;
-            }
-            if(j<n && s.charAt(j)!='0')
-            {res=(res%mod+(j-i+1)%mod);
-             //System.out.println(i+" "+j);
-            }
-            j++;
+            if(s.charAt(i)=='1')
+                cnt++;
+            else 
+                cnt=0;
+            res=(res+cnt)%mod;
         }
         return res;
     }
