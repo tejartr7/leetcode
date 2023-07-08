7673d4a96fd4f8d65bf9603725f4653884147e03
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-07-08 17:16:23 +0530

    Time: 29 ms (89.14%), Space: 44.7 MB (59.28%) - LeetHub

--- 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.java
+++ 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.java
@@ -3,25 +3,24 @@ class Solution {
         int i,n=s.length();
         long ans=0;
         Map<String,Long> map=new HashMap<>();
-        map.put("0",0l);
-        map.put("1",0l);
-        map.put("10",0l);
-        map.put("01",0l);
+        long a=0l;
+        long b,c,d;
+        b=c=d=a;
         //map.put("101",0l);
        // map.put("010",0l);
         for(i=0;i<n;i++)
         {
             if(s.charAt(i)=='1')
             {
-                ans+=map.get("10");
-                map.put("1",map.get("1")+1);
-                map.put("01",map.get("01")+map.get("0"));
+                ans+=d;
+                b+=1;
+                c+=a;
             }
             else
             {
-                ans+=map.get("01");
-                map.put("0",map.get("0")+1);
-                map.put("10",map.get("10")+map.get("1"));
+                ans+=c;
+                a+=1;
+                d+=b;
             }
         }
         return ans;
