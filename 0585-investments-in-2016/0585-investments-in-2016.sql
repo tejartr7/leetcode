46dd58c8622413a93c483e2195187298a0486254
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-07-20 10:38:41 +0530

    Time: 1170 ms (42.24%), Space: 0B (100.00%) - LeetHub

---
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+
+
+select round(sum(tiv_2016),2) as tiv_2016
+from insurance
+where tiv_2015 in 
+(select tiv_2015 
+from insurance
+group by tiv_2015
+having count(tiv_2015)>1)
+and (lat,lon) in 
+(select lat,lon 
+from insurance 
+group by lat,lon
+having count(*)=1)
+
+
