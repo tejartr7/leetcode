5973c2d678998ef98c44c7f14af37911a880f60a
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-08-11 10:12:07 +0530

    Time: 5 ms (71.02%), Space: 45.8 MB (39.59%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,15 +1,26 @@
 class Solution {
+    public int helper(int idx, int nums[], int amount, int dp[][]) {
+    if (idx == 0) {
+        if (amount % nums[idx] == 0)
+            dp[idx][amount] = 1;
+        else
+            dp[idx][amount] = 0;
+        return dp[idx][amount];
+    } 
+        if (dp[idx][amount] != -1)
+            return dp[idx][amount];
+        int not = helper(idx - 1, nums, amount, dp);
+        int take = 0;
+        if (amount >= nums[idx])
+            take = helper(idx, nums, amount - nums[idx], dp);
+        return dp[idx][amount] = take+not;
+    }
+
     public int change(int amount, int[] coins) {
-        int arr[] = new int[amount+1];
-        Arrays.fill(arr, 0);
-        arr[0] = 1;
-        for(int i:coins)
-        {
-            for(int j=i;j<=amount;j++)
-            {
-                arr[j]+=arr[j-i];
-            }
-        }
-        return arr[amount];
+        int i,j,n=coins.length;
+        int dp[][]=new int[n][amount+1];
+        for(i=0;i<n;i++)
+            Arrays.fill(dp[i],-1);
+        return helper(n-1,coins,amount,dp);
     }
 }
