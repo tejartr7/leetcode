fbcb9d8741eb08becd01d30fab7fe48e155adbd1
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-09-07 08:46:16 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (9.49%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -1,38 +1,22 @@
-class Solution {
+public class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        if(left == right){
-            return head;
+        ListNode temp = new ListNode(-1);
+        ListNode prev = temp;
+        temp.next = head;
+        
+        for (int i = 0; i < left - 1; i++) {
+            prev = prev.next;
         }
-
-        ListNode current = head;
-        ListNode prev = null;
-        // To skip till left we iterate till where we want our          reverse our linkedList
-        for(int i =0;current != null && i<left-1;i++){
-            prev = current;
-            current = current.next;
+        
+        ListNode cur = prev.next;
+        
+        for (int i = 0; i < right - left; i++) {
+            ListNode ptr = prev.next;
+            prev.next = cur.next;
+            cur.next = cur.next.next;
+            prev.next.next = ptr;
         }
-
-        ListNode last = prev;  // store this values which helps to point at the end which is 1 
-        ListNode newEnd = current; // newEnd = 2
-
-        // reverse the linkedList 
-        ListNode next = current.next;
-        for(int i =0;current != null && i<right - left +1;i++){
-            current.next = prev;
-            prev = current;
-            current = next;
-            if(next != null){
-                next = next.next;
-            }
-        }
-
-        if(last!= null){
-            last.next = prev;  // point last to prev which is 4 at this poiny
-        }else{
-            head = prev;
-        }
-
-        newEnd.next = current;  // point newEnd to current which is 5 at this point.
-        return head;
+        
+        return temp.next;
     }
 }
