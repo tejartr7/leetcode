9bbeba46234f0e55dad8f2a86fa927d1026c0454
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-09-08 21:12:12 +0530

    Time: 108 ms (44.99%), Space: 16.3 MB (78.93%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        def val(board, a, b, c):
+            cnt=0
+            for i in range(0,9):
+                if board[a][i] == c:
+                    cnt=cnt+1
+                if board[i][b] == c:
+                    cnt=cnt+1
+                if board[3 * (a // 3) + i // 3][3 * (b // 3) + i % 3] == c:
+                    cnt=cnt+1
+            if(cnt>3):
+                return False
+            return True
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] != '.':
+                    k = board[i][j]
+                    if not val(board, i, j, k):
+                        return False
+        return True
