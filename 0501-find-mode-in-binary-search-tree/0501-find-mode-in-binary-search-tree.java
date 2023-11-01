0b077e2c9d562e0d74e0747198232163f1f6909b
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-11-01 20:30:24 +0530

    Time: 7 ms (31.79%), Space: 44.2 MB (47.04%) - LeetHub

---
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] findMode(TreeNode root) {
+        Map<Integer, Integer> counter = new HashMap();
+        dfs(root, counter);
+        int maxFreq = 0;
+        
+        for (int key : counter.keySet()) {
+            maxFreq = Math.max(maxFreq, counter.get(key));
+        }
+        
+        List<Integer> ans = new ArrayList();
+        for (int key : counter.keySet()) {
+            if (counter.get(key) == maxFreq) {
+                ans.add(key);
+            }
+        }
+        
+        int[] result = new int[ans.size()];
+        for (int i = 0; i < ans.size(); i++) {
+            result[i] = ans.get(i);
+        }
+        
+        return result;
+    }
+    
+    public void dfs(TreeNode node, Map<Integer, Integer> counter) {
+        if (node == null) {
+            return;
+        }
+        
+        counter.put(node.val, counter.getOrDefault(node.val, 0) + 1);
+        dfs(node.left, counter);
+        dfs(node.right, counter);
+    }
+}
